6ada97d895da683dfa5ed4d9be32823e67c177ca
SergeyPodgorny <serkonpod@gmail.com>
2022-06-24 20:14:56 +0300

    initial commit for orders-service

---
+++ orders-service/src/main/java/com/fnd/games_store/orders/OrdersApplication.java
@@ -0,0 +1,15 @@
+package com.fnd.games_store.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrdersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersApplication.class,args);
+    }
+
+}
