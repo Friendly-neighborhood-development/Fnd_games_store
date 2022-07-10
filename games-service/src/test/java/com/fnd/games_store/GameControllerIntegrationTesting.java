b1d8b758e15a863b0dcf5138d5c339b680544699
SergeyPodgorny <serkonpod@gmail.com>
2022-07-10 09:11:59 +0300

    Mockmvc testing deployed

--- games-service/src/test/java/com/fnd/games_store/GameControllerIntegrationTesting.java
+++ games-service/src/test/java/com/fnd/games_store/GameControllerIntegrationTesting.java
@@ -3,12 +3,9 @@ package com.fnd.games_store;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fnd.games_store.games.GamesApplication;
 import com.fnd.games_store.games.controller.dto.GameResponseDTO;
-import com.fnd.games_store.games.entity.Game;
-import com.fnd.games_store.games.repository.GameRepository;
 import com.fnd.games_store.games.service.GameService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
@@ -19,8 +16,6 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import org.springframework.test.web.servlet.MvcResult;
 
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -37,9 +32,6 @@ public class GameControllerIntegrationTesting extends TestUtilities{
     MockMvc mvc;
 
 
-    @Autowired
-    GameRepository gameRepository;
-
     @MockBean
     GameService gameService;
 
@@ -48,65 +40,28 @@ public class GameControllerIntegrationTesting extends TestUtilities{
 
     @Test
     void test1() throws Exception {
+
+
+
         MvcResult requestResult = this.mvc.perform(post("/create").
-                content(createGameRequest).
+                content(createGameControllerRequest(createAppropriateGameRequestDTO())).
                 contentType(MediaType.APPLICATION_JSON).
                 accept(MediaType.APPLICATION_JSON)).
                 andExpect(status().isOk()).
                 andReturn();
         String body = requestResult.getResponse().getContentAsString();
-        ObjectMapper objectMapper = new ObjectMapper();
-        GameResponseDTO gameResponseDTO = objectMapper.readValue(body, GameResponseDTO.class);
-        List <Game> createdDuringTestingGame = gameRepository.findAll();
-
-
-        assertThat(createdDuringTestingGame.get(0).getName()).isEqualTo("name");
-
-
-
-
-
-
-
-
-//        assertThat(gameResponseDTO.getName()).isEqualTo("name");
-//        assertThat(gameResponseDTO.getGenre()).isEqualTo("genre");
-//        assertThat(gameResponseDTO.getReleaseDate()).isEqualTo("date");
-//        assertThat(gameResponseDTO.getDeveloper()).isEqualTo("developer");
-//        assertThat(gameResponseDTO.getPublisher()).isEqualTo("publisher");
-//        assertThat(gameResponseDTO.getPlatform()).isEqualTo("platform");
-//        assertThat(gameResponseDTO.getFeatures()).isEqualTo("features");
-//        assertThat(gameResponseDTO.getPrice().toBigInteger()).isEqualTo(1000);
-//        assertThat(gameResponseDTO.getDiscount().toBigInteger()).isEqualTo(200);
-//        assertThat(gameResponseDTO.getDescription()).isEqualTo(null);
+        GameResponseDTO gameResponseDTO = new ObjectMapper().readValue(body, GameResponseDTO.class);
+        assertThat(gameResponseDTO).isEqualTo(createAppropriateGameResponseDTO());
 
 
     }
-
-
-//    gameResponseDTO.setId("1");
-//        gameResponseDTO.setName(null);
-//        gameResponseDTO.setGenre(null);
-//        gameResponseDTO.setReleaseDate("date");
-//        gameResponseDTO.setDeveloper("developer");
-//        gameResponseDTO.setPublisher("publisher");
-//        gameResponseDTO.setPlatform("platform");
-//        gameResponseDTO.setFeatures("features");
-//        gameResponseDTO.setPrice(BigDecimal.valueOf(1000));
-//        gameResponseDTO.setDiscount(BigDecimal.valueOf(200));
-//        gameResponseDTO.setDescription("description");
-
 
 
     @BeforeEach
     private void createRequestIntegrationTestSetUp(){
-        when(gameService.createGameEntry(ArgumentMatchers.any())).thenReturn(createAppropriateGameResponseDTO());
+        when(gameService.createGameEntry(createAppropriateGameRequestDTO()))
+                .thenReturn(createCustomGameResponseDTO(createAppropriateGameRequestDTO()));
     }
-
-
-
-
-
 
 
 }
--- games-service/src/test/java/com/fnd/games_store/TestUtilities.java
+++ games-service/src/test/java/com/fnd/games_store/TestUtilities.java
@@ -1,5 +1,7 @@
 package com.fnd.games_store;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fnd.games_store.games.controller.dto.GameRequestDTO;
 import com.fnd.games_store.games.controller.dto.GameResponseDTO;
 import org.jetbrains.annotations.NotNull;
@@ -10,6 +12,8 @@ import java.util.List;
 
 public class TestUtilities {
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
 
     protected final String getGameRequest =
             "{"+"\"name\": \"gta5\""+"}";
@@ -17,16 +21,6 @@ public class TestUtilities {
 
 
     protected final String getGameCatalogueRequest = "";
-
-    protected final String createGameRequest = "{"+"" +
-            "\"name\": \"gta5\""
-
-
-
-
-
-
-            +"}";
 
     protected final String updateGameRequest = "";
 
@@ -36,6 +30,35 @@ public class TestUtilities {
     protected TestUtilities(){
 
     }
+
+
+
+    protected @NotNull String createGameControllerRequest(GameRequestDTO gameRequestDTO) {
+        createCustomGameResponseDTO(gameRequestDTO);
+        try{
+            return objectMapper.writeValueAsString(createAppropriateGameResponseDTO());
+        } catch (JsonProcessingException e){
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    protected @NotNull GameResponseDTO createCustomGameResponseDTO(GameRequestDTO gameRequestDTO){
+        GameResponseDTO gameResponseDTO = new GameResponseDTO();
+        gameResponseDTO.setId(gameRequestDTO.getId());
+        gameResponseDTO.setName(gameRequestDTO.getName());
+        gameResponseDTO.setGenre(gameRequestDTO.getGenre());
+        gameResponseDTO.setReleaseDate(gameRequestDTO.getReleaseDate());
+        gameResponseDTO.setDeveloper(gameRequestDTO.getDeveloper());
+        gameResponseDTO.setPublisher(gameRequestDTO.getPublisher());
+        gameResponseDTO.setPlatform(gameRequestDTO.getPlatform());
+        gameResponseDTO.setFeatures(gameRequestDTO.getFeatures());
+        gameResponseDTO.setPrice(gameRequestDTO.getPrice());
+        gameResponseDTO.setDiscount(gameRequestDTO.getDiscount());
+        gameResponseDTO.setDescription(gameRequestDTO.getDescription());
+        return gameResponseDTO;
+    }
+
 
     protected @NotNull GameResponseDTO createAppropriateGameResponseDTO(){
         GameResponseDTO gameResponseDTO = new GameResponseDTO();
@@ -92,7 +115,21 @@ public class TestUtilities {
         return gameRequestDTO;
     }
 
-
+    protected @NotNull GameRequestDTO createNotAppropriateGameRequestDTO(){
+        GameRequestDTO gameRequestDTO = new GameRequestDTO();
+        gameRequestDTO.setId("1");
+        gameRequestDTO.setName(null);
+        gameRequestDTO.setGenre(null);
+        gameRequestDTO.setReleaseDate("date");
+        gameRequestDTO.setDeveloper("developer");
+        gameRequestDTO.setPublisher("publisher");
+        gameRequestDTO.setPlatform("platform");
+        gameRequestDTO.setFeatures("features");
+        gameRequestDTO.setPrice(BigDecimal.valueOf(1000));
+        gameRequestDTO.setDiscount(BigDecimal.valueOf(200));
+        gameRequestDTO.setDescription("description");
+        return gameRequestDTO;
+    }
 
 
 }
