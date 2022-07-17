2d4d51f86389bed36aed8108ac9b35cc06a5fe2b
SergeyPodgorny <serkonpod@gmail.com>
2022-07-17 19:16:28 +0300

    added few profiles to services

--- games-service/src/main/resources/db/migration/V1__create_games_catalogue_table.sql
+++ games-service/src/main/resources/db/migration/V1__create_games_catalogue_table.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS games_catalogue
 (id varchar(255) PRIMARY KEY UNIQUE NOT NULL,
 name varchar(20) UNIQUE NOT NULL,
-genre varchar(20) UNIQUE NOT NULL,
-releasedate varchar(20) UNIQUE NOT NULL,
-developer varchar(20) UNIQUE NOT NULL,
-publisher varchar(20) UNIQUE NOT NULL,
-platform varchar(20) UNIQUE NOT NULL,
-features varchar(20) UNIQUE NOT NULL,
-price numeric(40,2) UNIQUE NOT NULL,
-discount numeric (40,2) UNIQUE NOT NULL,
-description varchar(20) UNIQUE NOT NULL);
+genre varchar(20) NOT NULL,
+releasedate varchar(20) NOT NULL,
+developer varchar(20) NOT NULL,
+publisher varchar(20) NOT NULL,
+platform varchar(20) NOT NULL,
+features varchar(20) NOT NULL,
+price money NOT NULL,
+discount money NOT NULL,
+description varchar(255) NOT NULL);
 
 
