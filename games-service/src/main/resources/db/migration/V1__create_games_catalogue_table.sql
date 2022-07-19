f82d7c38e71e2fafd303d467f45665900ce9895c
SergeyPodgorny <serkonpod@gmail.com>
2022-07-19 20:23:49 +0300

    migrations file fixed

--- games-service/src/main/resources/db/migration/V1__create_games_catalogue_table.sql
+++ games-service/src/main/resources/db/migration/V1__create_games_catalogue_table.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS games_catalogue
-(id varchar(255) PRIMARY KEY UNIQUE NOT NULL,
+(game_id varchar(255) PRIMARY KEY UNIQUE NOT NULL,
 name varchar(20) UNIQUE NOT NULL,
 genre varchar(20) NOT NULL,
-releasedate varchar(20) NOT NULL,
+release_date varchar(20) NOT NULL,
 developer varchar(20) NOT NULL,
 publisher varchar(20) NOT NULL,
 platform varchar(20) NOT NULL,
 features varchar(20) NOT NULL,
-price numeric(4,2) NOT NULL,
-discount numeric(4,2) NOT NULL,
+price numeric(10,2) NOT NULL,
+discount numeric(10,2) NOT NULL,
 description varchar(255) NOT NULL);
 
 
