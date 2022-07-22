0e549ada2dfb53e90c490c502605739b457d2b66
Andrew-Sem <avvyayvayva@gmail.com>
2022-07-22 22:26:07 +0300

    update game card styles

--- client/src/components/GameCard.tsx
+++ client/src/components/GameCard.tsx
@@ -11,20 +11,25 @@ interface GameCardProps {
 
 const GameCard: FC<gameProps> = ({name, description, base64Image, price, discount}) => {
     return (
-        <div className={"flex flex-col w-56 p-2.5 mb-4 mx-2 rounded-3xl bg-white"}>
-            <div className={"max-h-64 w-full rounded-2xl overflow-hidden flex justify-center items-center mb-4"}>
+        <div className={"flex flex-col w-56 mb-4 mx-2git  bg-white flex justify-between"}>
+            <div className={"max-h-56 w-full overflow-hidden flex justify-center items-center mb-4"}>
                 <img className={"w-full"} src={base64Image} alt={"game picture"}/>
             </div>
-            <div className={"mx-2"}>
-                <h3 className={"mb-2"}>{name}</h3>
-                <div  className={"text-sm"}>
-                    {discount
-                        ? <div>
-                            <span className={"line-through text-gray-400"}>RUB {price}</span>
-                            <span>  RUB {price - discount}</span>
+            <div className={" px-1 pb-2"}>
+                <h3 className={"mb-2 text-ellipsis whitespace-nowrap overflow-hidden"}>{name}</h3>
+                <div className={"text-sm"}>
+                    {price===0
+                        ?<div>Бесплатно</div>
+                        :(discount
+                        ? <div className={"flex justify-between items-center"}>
+                            <span className={"flex items-center p-2 bg-blue-600 text-white rounded-lg text-2xs"}>-{discount}%</span>
+                            <div className={"ml-1"}>
+                                <div className={"line-through text-gray-400"}>RUB {price}</div>
+                                <div>  RUB {(price - price*discount/100).toFixed(2)}</div>
+                            </div>
+
                         </div>
-                        : <span>RUB {price}</span>}
-                    {/*<CustomButton>Получить</CustomButton>*/}
+                        : <div>RUB {price}</div>)}
                 </div>
             </div>
 
