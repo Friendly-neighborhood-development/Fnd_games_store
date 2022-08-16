4d09012835518b4250511a38a0fcab21aa60d6bc
Andrew-Sem <avvyayvayva@gmail.com>
2022-08-16 22:55:47 +0300

    added hover effect to navbar icon

--- client/src/components/UI/NavbarIcon.tsx
+++ client/src/components/UI/NavbarIcon.tsx
@@ -4,30 +4,20 @@ import {BellIcon} from "@heroicons/react/outline";
 
 interface NavbarIconProps {
     Icon: any,
-    link?: string,
+    link: string,
 }
 
 const NavbarIcon: FC<NavbarIconProps> = ({Icon, link}) => {
     return (
-        <>
-            {link
-                ? <Link to={link}>
-                    <span
-                        className={"rounded-full bg-white border-solid border-gray-300 border" +
-                            " h-10 w-10 flex justify-center items-center dark:bg-slate-700/50" +
-                            " dark:border-slate-600"}
-                    >
-                        <Icon className={"text-gray-500 dark:text-slate-400 w-3/5 h-3/5"}/>
-                    </span>
-                </Link>
-                : <span
-                    className={"rounded-full bg-white border-solid border-gray-300 border" +
-                        " h-10 w-10 flex justify-center items-center dark:bg-slate-700/50" +
-                        " dark:border-slate-600 cursor-pointer"}
-                >
-                        <Icon className={"text-gray-500 dark:text-slate-400 w-3/5 h-3/5"}/>
-                    </span>}
-        </>
+        <Link to={link} className={""}>
+            <span
+                className={"rounded-full bg-white border-solid border-gray-300 border" +
+                    " h-10 w-10 flex justify-center items-center dark:bg-slate-700/50" +
+                    " dark:border-slate-600 dark:hover:bg-slate-500/50"}
+            >
+                <Icon className={"text-gray-500 dark:text-slate-400 w-3/5 h-3/5"}/>
+            </span>
+        </Link>
     );
 };
 
