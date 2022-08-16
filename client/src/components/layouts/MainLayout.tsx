66dedad2f860a0be64c1064844c3fd4e83a3f59d
Andrew-Sem <avvyayvayva@gmail.com>
2022-08-16 22:39:31 +0300

    small refactoring

--- client/src/components/layouts/MainLayout.tsx
+++ client/src/components/layouts/MainLayout.tsx
@@ -8,13 +8,16 @@ interface MainLayoutProps {
 
 const MainLayout: FC<MainLayoutProps> = ({children}) => {
     return (
-        <div className="container mx-auto min-h-screen">
-            {/*<Sidebar/>*/}
-            <div className="flex flex-col items-center pt-4 lg:pt-6">
-                <Navbar/>
-                {children}
+        <div className="text-slate-800 dark:text-slate-400 bg-gray-100 dark:bg-slate-900 flex">
+            <div className="container mx-auto min-h-screen">
+                {/*<Sidebar/>*/}
+                <div className="flex flex-col items-center pt-4 lg:pt-6">
+                    <Navbar/>
+                    {children}
+                </div>
             </div>
         </div>
+
     );
 };
 
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -8,41 +8,37 @@ import Button from "../components/UI/Button";
 const Login = () => {
     return (
         <NotLoginLayout>
-            <div className={"flex justify-center"}>
-                <div className={"pt-6 flex flex-col items-center w-80"}>
-                    <Link to={"/games"} className={"my-4"}>
-                        <PuzzleIcon className={"w-16 h-16 text-sky-500"}/>
+            <div className={"pt-6 w-80 flex flex-col items-center"}>
+                <Link to={"/games"} className={"my-4"}>
+                    <PuzzleIcon className={"w-16 h-16 text-sky-500"}/>
+                </Link>
+                <h1 className={"text-xl text-slate-300 mb-4"}>
+                    Sign in to Games Store
+                </h1>
+                <form
+                    className={"p-4 dark:bg-slate-700/50 dark:text-slate-300 rounded-md w-full space-y-2 flex flex-col border-gray-500/30 border"}>
+                    <Input
+                        type={"email"}
+                        label={"Email"}
+                        className={"rounded p-1 my-2 border-gray-500/50 border"}
+                    />
+                    <Input
+                        type={"password"}
+                        label={"Password"}
+                        className={"rounded p-1 my-2 border-gray-500/50 border"}
+                    />
+                    <Button className={"dark:bg-green-700 dark:hover:bg-green-600 w-full dark:text-white"}>
+                        Sign in
+                    </Button>
+                </form>
+                <div className={"text-sm p-4 rounded-lg border border-gray-500/50 w-full mt-4 flex justify-center"}>
+                    New to Games Store?
+                    <Link to={"/register"} className={"ml-1 text-sky-500"}>
+                        Create an account
                     </Link>
-                    <h1 className={"text-xl text-slate-300 mb-4"}>
-                        Sign in to Games Store
-                    </h1>
-                    <form
-                        className={"p-4 dark:bg-slate-700/50 dark:text-slate-300 rounded-md w-full space-y-2 flex flex-col border-gray-500/30 border"}>
-                        <Input
-                            type={"email"}
-                            label={"Email"}
-                            className={"rounded p-1 my-2 border-gray-500/50 border"}
-                        />
-                        <Input
-                            type={"password"}
-                            label={"Password"}
-                            className={"rounded p-1 my-2 border-gray-500/50 border"}
-                        />
-                        <Button className={"dark:bg-green-700 dark:hover:bg-green-600 w-full dark:text-white"}>
-                            Sign in
-                        </Button>
-                    </form>
-                    <div className={"text-sm p-4 rounded-lg border border-gray-500/50 w-full mt-4 flex justify-center"}>
-                        New to Games Store?
-                        <Link to={"/register"} className={"ml-1 text-sky-500"}>
-                            Create an account
-                        </Link>
-                    </div>
                 </div>
             </div>
-
         </NotLoginLayout>
-
     );
 };
 
