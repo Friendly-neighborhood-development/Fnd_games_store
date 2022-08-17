c8c170929578194e714d44dac53d7d6a5b7dfba6
Andrew-Sem <avvyayvayva@gmail.com>
2022-08-17 14:56:07 +0300

    little design update

--- client/src/components/UI/Button.tsx
+++ client/src/components/UI/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 }
 
 const Button: FC<ButtonProps> = ({className, children, ...props}) => {
-    const rootClasses = "px-6 py-2 text-sm self-center cursor-pointer rounded " + className
+    const rootClasses = "px-6 py-2 text-sm self-center cursor-pointer rounded dark:text-slate-200 " + className
     return (
         <button className={rootClasses} {...props}>
             {children}
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -15,11 +15,11 @@ const Login = () => {
                 <Link to={"/games"} className={"my-4"}>
                     <PuzzleIcon className={"w-16 h-16 text-blue-600 dark:text-sky-500"}/>
                 </Link>
-                <h1 className={"text-xl dark:text-slate-300 mb-4"}>
+                <h1 className={"text-xl dark:text-slate-200 mb-4"}>
                     Sign in to Games Store
                 </h1>
                 <form
-                    className={"p-4 bg-white dark:bg-slate-700/50 dark:text-slate-300 rounded-md w-full space-y-2 flex flex-col border-gray-500/30 border"}
+                    className={"p-4 bg-white dark:bg-slate-700/50 dark:text-slate-300 rounded-md w-full space-y-2 flex flex-col border-gray-500/50 border"}
                     onSubmit={formHandler}
                 >
                     <Input
@@ -32,19 +32,18 @@ const Login = () => {
                         label={"Password"}
                         className={"rounded-md py-1.5 my-2 border-gray-500/50 border"}
                     />
-                    <button />
                     <Button
-                        className={"bg-green-600 dark:bg-green-700 hover:bg-green-500 dark:hover:bg-green-600 w-full text-white"}
+                        className={"bg-green-500 dark:bg-green-700 hover:bg-green-600 dark:hover:bg-green-600 w-full text-white"}
                         type={"submit"}
                         // value={"Submit"}
                     >
                         Sign in
                     </Button>
                 </form>
-                <div className={"text-sm bg-white dark:bg-transparent p-4 rounded-lg border border-gray-500/30 dark:border-gray-500/50 w-full mt-4 flex justify-center"}>
-                    New to Games Store?
+                <div className={"text-sm bg-white dark:bg-transparent p-4 rounded-lg border border-gray-500/50 dark:border-gray-500/50 w-full mt-4 flex justify-center"}>
+                    <span className={"dark:text-slate-200"}>New to Games Store?</span>
                     <Link to={"/register"} className={"ml-1 text-blue-600 dark:text-sky-500"}>
-                        Create an account
+                        Create an account.
                     </Link>
                 </div>
             </div>
