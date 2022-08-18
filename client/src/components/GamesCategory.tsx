787b4c15176cb0e351e940342206c630f3ea3d5c
Andrew-Sem <avvyayvayva@gmail.com>
2022-08-18 15:01:07 +0300

    hover anim games category title

--- client/src/components/GamesCategory.tsx
+++ client/src/components/GamesCategory.tsx
@@ -3,7 +3,7 @@ import GameCard from "./GameCard";
 import {game} from "../types/Games";
 import {ChevronRightIcon} from "@heroicons/react/outline";
 
-interface GamesCategoryProps{
+interface GamesCategoryProps {
     title: string
     games: Array<game>
 }
@@ -11,10 +11,12 @@ interface GamesCategoryProps{
 const GamesCategory: FC<GamesCategoryProps> = memo(({title, games}) => {
     return (
         <section className={"w-full"}>
-            <div className={"flex justify-start w-full items-center my-4 cursor-pointer"}>
-                <span className={"text-xl dark:text-slate-100 ml-4"}>{title}</span>
-                <span className={"ml-1"}>
-                    <ChevronRightIcon className={"w-4 h-4 text-slate-800 dark:text-slate-100"}/>
+            <div className={"flex justify-start my-4 "}>
+                <span className={"flex items-center cursor-pointer hover:text-blue-600 dark:hover:text-sky-500 dark:text-slate-100"}>
+                    <span className={"text-xl ml-4"}>{title}</span>
+                    <span className={"ml-1"}>
+                        <ChevronRightIcon className={"w-4 h-4 "}/>
+                    </span>
                 </span>
             </div>
             <div className={"flex overflow-x-auto lg:flex-wrap justify-between w-full"}>
