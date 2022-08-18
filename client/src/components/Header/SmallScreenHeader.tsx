ed896985551cedf493fb5717943c2d21c7ac3dfa
Andrew-Sem <avvyayvayva@gmail.com>
2022-08-18 20:43:22 +0300

    header position fix

--- client/src/components/Header/SmallScreenHeader.tsx
+++ client/src/components/Header/SmallScreenHeader.tsx
@@ -1,15 +1,14 @@
 import React, {FC} from 'react';
 import {MenuIcon, PuzzleIcon, SearchIcon} from "@heroicons/react/outline";
-import Search from "../UI/Search";
 import {Link} from "react-router-dom";
 
 
 const SmallScreenHeader: FC = () => {
     return (
         <header
-            className={"w-full border-b border-slate-500/30 sticky top-0 z-10 bg-white dark:backdrop-blur dark:bg-slate-900/80 shadow-lg dark:shadow-none shadow-gray-500/10"}>
+            className={"w-full border-b border-slate-500/30 lg:hidden sticky top-0 z-10 bg-white dark:backdrop-blur dark:bg-slate-900/80 shadow-lg dark:shadow-none shadow-gray-500/10"}>
             <div className="container mx-auto">
-                <div className={"w-full justify-between items-center flex lg:hidden py-3"}>
+                <div className={"w-full justify-between items-center flex  py-3"}>
                     <SearchIcon className={"w-6 h-6 dark:text-gray-100"}/>
                     <Link to={"/games"} className={"flex items-center"}>
                         <PuzzleIcon className={"w-6 h-6 text-blue-500"}/>
