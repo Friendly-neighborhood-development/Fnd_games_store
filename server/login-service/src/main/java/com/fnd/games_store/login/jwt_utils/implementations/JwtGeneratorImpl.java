498f7643dce84b38865a1ca599651061038c8080
SergeyPodgorny <serkonpod@gmail.com>
2022-09-01 21:32:10 +0300

    properties files configured for tests and written test for checking values of secret and expiration duration of a jwt token

--- server/login-service/src/main/java/com/fnd/games_store/login/jwt_utils/implementations/JwtGeneratorImpl.java
+++ server/login-service/src/main/java/com/fnd/games_store/login/jwt_utils/implementations/JwtGeneratorImpl.java
@@ -1,11 +1,18 @@
 package com.fnd.games_store.login.jwt_utils.implementations;
 
 import com.fnd.games_store.login.jwt_utils.JwtGenerator;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.security.RolesAllowed;
+
 @Component
+@NoArgsConstructor
+@AllArgsConstructor
 public class JwtGeneratorImpl implements JwtGenerator {
 
     @Value("${variables.security.access_secret}")
@@ -18,4 +25,14 @@ public class JwtGeneratorImpl implements JwtGenerator {
     public String generateToken(UserDetails userDetails) {
         return null;
     }
+
+    @Profile("test")
+    public String getSecret() {
+        return secret;
+    }
+    @Profile("test")
+    public String getExpirationDuration() {
+        return expirationDuration;
+    }
 }
+
--- server/login-service/src/test/java/com/fnd/games_store/test/jwt_utils/JwtGeneratorTest.java
+++ server/login-service/src/test/java/com/fnd/games_store/test/jwt_utils/JwtGeneratorTest.java
@@ -1,7 +1,9 @@
 package com.fnd.games_store.test.jwt_utils;
 
 import com.fnd.games_store.login.LoginApplication;
-import com.fnd.games_store.login.jwt_utils.JwtGenerator;
+import com.fnd.games_store.login.jwt_utils.implementations.JwtGeneratorImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,19 +13,27 @@ import java.lang.reflect.Field;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = LoginApplication.class)
+@Slf4j
 public class JwtGeneratorTest {
 
     @Autowired
-    JwtGenerator JwtGeneratorImpl;
+    JwtGeneratorImpl jwtGeneratorImpl;
+
+    private String injectedSecret;
+
+    private String injectedExpiration;
 
     @Test
-    void doesTheFieldInjects(){
-        Field[] fields = JwtGeneratorImpl.getClass().getDeclaredFields();
-        for(Field field: fields){
-            assertThat(field).isNotNull();
-        }
-
-
+    void doesInjectedFieldsHaveCorrectValues() throws NoSuchFieldException, IllegalAccessException {
+        assertThat(injectedSecret).isEqualTo("OTFBRjg3MUMwOUYxODlFNDc3NEQwNTAxMTg0M0M0NzI5QTczNDY1QUU4MTA5MUFERjZFNDIxNTk0QTZDOUYyQg==");
+        assertThat(injectedExpiration).isEqualTo("90000000");
     }
+
+    @BeforeEach
+    private void injectedFiledValuesAccessSetUp() {
+        injectedSecret = jwtGeneratorImpl.getSecret();
+        injectedExpiration = jwtGeneratorImpl.getExpirationDuration();
+    }
+
 
 }
