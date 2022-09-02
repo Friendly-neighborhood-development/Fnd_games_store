51ae26bd553dce29fe1f2e23a4ebc6ce7a61c3f9
SergeyPodgorny <serkonpod@gmail.com>
2022-09-02 21:23:13 +0300

    added exception handling to authentication

--- server/login-service/src/main/java/com/fnd/games_store/login/service/LoginService.java
+++ server/login-service/src/main/java/com/fnd/games_store/login/service/LoginService.java
@@ -2,6 +2,6 @@ package com.fnd.games_store.login.service;
 
 public interface LoginService {
 
-    public String login(String username,String password);
+    public String login(String username,String password) throws Exception;
 
 }
--- server/login-service/src/main/java/com/fnd/games_store/login/service/implementation/LoginServiceImpl.java
+++ server/login-service/src/main/java/com/fnd/games_store/login/service/implementation/LoginServiceImpl.java
@@ -6,19 +6,22 @@ import com.fnd.games_store.login.jwt_utils.implementation.UserDetailsServiceImpl
 import com.fnd.games_store.login.service.LoginService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
 
 @Service
 @Slf4j
 public class LoginServiceImpl implements LoginService {
 
+    private final AuthenticationManager authenticationManager;
     private final UserDetailsServiceImpl userDetails;
     private final JwtGeneratorImpl jwtGenerator;
     @Autowired
-    public LoginServiceImpl(UserDetailsServiceImpl userDetails, JwtGeneratorImpl jwtGenerator) {
+    public LoginServiceImpl(AuthenticationManager authenticationManager, UserDetailsServiceImpl userDetails, JwtGeneratorImpl jwtGenerator) {
+        this.authenticationManager = authenticationManager;
         this.userDetails = userDetails;
         this.jwtGenerator = jwtGenerator;
     }
@@ -26,9 +29,13 @@ public class LoginServiceImpl implements LoginService {
     @Override
     public String login(String username,String password) {
 
+        try{
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (BadCredentialsException e){
+            throw new RuntimeException("INVALID_CREDENTIALS", e);
+        }
 
-
-
+        log.info(SecurityContextHolder.getContext().getAuthentication().toString());
 
         return jwtGenerator.generateJwtToken(userDetails.loadUserByUsername(username));
     }
