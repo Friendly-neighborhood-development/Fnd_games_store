579c1bbf2a86640196854c378fe5aa9bc9491817
SergeyPodgorny <serkonpod@gmail.com>
2022-09-02 19:28:40 +0300

    added second sql migration

--- server/login-service/src/main/resources/db/migration/V1__create_account_and_authority_tables.sql
+++ server/login-service/src/main/resources/db/migration/V1__create_account_and_authority_tables.sql
@@ -1,3 +1,8 @@
+CREATE TABLE IF NOT EXISTS authorities
+(authority_id varchar PRIMARY KEY UNIQUE NOT NULL,
+  authority varchar(50) UNIQUE NOT NULL);
+
+
 CREATE TABLE IF NOT EXISTS accounts
 (account_id varchar PRIMARY KEY UNIQUE NOT NULL,
 username varchar(50)  UNIQUE NOT NULL,
@@ -8,8 +13,6 @@ FOREIGN KEY(authority_id)
 REFERENCES authorities(authority_id));
 
 
-CREATE TABLE IF NOT EXISTS authorities
-(authority_id varchar PRIMARY KEY UNIQUE NOT NULL,
-authority varchar(50) UNIQUE NOT NULL);
+
 
 
