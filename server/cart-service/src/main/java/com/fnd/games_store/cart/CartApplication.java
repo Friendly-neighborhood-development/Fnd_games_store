4e5bc57ad155ff0480d55507437460d2941c1cc3
SergeyPodgorny <serkonpod@gmail.com>
2022-09-04 08:35:20 +0300

    initial commit for cart-service

---
+++ server/cart-service/src/main/java/com/fnd/games_store/cart/CartApplication.java
@@ -0,0 +1,17 @@
+package com.fnd.games_store.cart;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartApplication.class,args);
+    }
+
+
+}
