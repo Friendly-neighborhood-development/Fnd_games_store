7ffb9e03269f26a8a576808a0492992e25eb3116
SergeyPodgorny <serkonpod@gmail.com>
2022-09-04 09:47:47 +0300

    added lombok and dto objects

---
+++ server/gateway-service/src/main/java/com/fnd/games_store/gateway/dto/UserValidationRequestDTO.java
@@ -0,0 +1,12 @@
+package com.fnd.games_store.gateway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserValidationRequestDTO {
+
+    private String token;
+
+}
---
+++ server/gateway-service/src/main/java/com/fnd/games_store/gateway/dto/UserValidationResponseDTO.java
@@ -0,0 +1,12 @@
+package com.fnd.games_store.gateway.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserValidationResponseDTO {
+
+    private Boolean isUserValid;
+
+}
