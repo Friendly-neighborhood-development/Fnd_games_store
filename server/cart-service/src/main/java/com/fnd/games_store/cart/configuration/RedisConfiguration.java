5a3b3197c744ec0dd8f541a734ed2c65308f1aa0
SergeyPodgorny <serkonpod@gmail.com>
2022-09-07 19:20:43 +0300

    added injectable values of route and port to redis configuration

--- server/cart-service/src/main/java/com/fnd/games_store/cart/configuration/RedisConfiguration.java
+++ server/cart-service/src/main/java/com/fnd/games_store/cart/configuration/RedisConfiguration.java
@@ -1,25 +1,33 @@
 package com.fnd.games_store.cart.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfiguration {
 
+    @Value("${variables.redis.host}")
+    private String redisRoute;
 
-    @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
-        return new JedisConnectionFactory(config);
-    }
+    @Value("${variables.redis.port}")
+    private int redisPort;
+
+
+
+	@Bean
+	public LettuceConnectionFactory lettuceConnectionFactory() {
+		  return new LettuceConnectionFactory(new RedisStandaloneConfiguration(redisRoute, redisPort));
+	 }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(lettuceConnectionFactory());
         return template;
     }
 
