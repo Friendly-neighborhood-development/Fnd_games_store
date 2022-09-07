301d3d7480321d8afeb40bd7297109b986cd3327
SergeyPodgorny <serkonpod@gmail.com>
2022-09-07 19:10:29 +0300

    added configuration ad removed session dependency for redis session

---
+++ server/cart-service/src/main/java/com/fnd/games_store/cart/configuration/RedisConfiguration.java
@@ -0,0 +1,30 @@
+package com.fnd.games_store.cart.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfiguration {
+
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("localhost", 6379);
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+
+
+
+
+
+}
