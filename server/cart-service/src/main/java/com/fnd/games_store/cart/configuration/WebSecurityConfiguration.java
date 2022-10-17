8f6434060dae65dee5fa72370b099b7d8397af97
SergeyPodgorny <serkonpod@gmail.com>
2022-10-17 16:54:56 +0300

    added authentication manager bean to configuration

--- server/cart-service/src/main/java/com/fnd/games_store/cart/configuration/WebSecurityConfiguration.java
+++ server/cart-service/src/main/java/com/fnd/games_store/cart/configuration/WebSecurityConfiguration.java
@@ -2,12 +2,14 @@ package com.fnd.games_store.cart.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration {
+public class WebSecurityConfiguration {
 
 
     @Bean
@@ -20,6 +22,9 @@ public class SecurityConfiguration {
         return http.build();
     }
 
-
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
 }
