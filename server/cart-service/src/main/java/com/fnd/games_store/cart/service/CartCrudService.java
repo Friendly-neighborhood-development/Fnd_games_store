8856a1ec421be71414736661b21a4200e3f0b1c9
SergeyPodgorny <serkonpod@gmail.com>
2022-10-19 17:48:55 +0300

    primary service interface CartCrudService refactored before working on implementation

--- server/cart-service/src/main/java/com/fnd/games_store/cart/service/CartCrudService.java
+++ server/cart-service/src/main/java/com/fnd/games_store/cart/service/CartCrudService.java
@@ -2,19 +2,13 @@ package com.fnd.games_store.cart.service;
 
 import com.fnd.games_store.cart.entity.Cart;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CartCrudService {
 
-
-    public void persistCart(Cart cart);
-
-    public Optional<Cart> getCartById(String cartId);
-
-    public void deleteCartById(String cartId);
-
     public void updateCart(Cart cart);
-
-
+    public Optional<Cart> getCartContent(String userId);
+    public void deleteGameEntry(String userId, String gameId);
 
 }
--- server/cart-service/src/main/java/com/fnd/games_store/cart/service/implementation/CartService.java
+++ server/cart-service/src/main/java/com/fnd/games_store/cart/service/implementation/CartService.java
@@ -4,6 +4,7 @@ import com.fnd.games_store.cart.entity.Cart;
 import com.fnd.games_store.cart.repository.RedisRepository;
 import com.fnd.games_store.cart.service.CartCrudService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -13,18 +14,18 @@ public class CartService implements CartCrudService {
 
     private RedisRepository redisRepository;
 
-    @Override
-    public void persistCart(Cart cart) {
-
+    @Autowired
+    public CartService(RedisRepository redisRepository) {
+        this.redisRepository = redisRepository;
     }
 
     @Override
-    public Optional<Cart> getCartById(String cartId) {
+    public Optional<Cart> getCartContent(String cartId) {
         return Optional.empty();
     }
 
     @Override
-    public void deleteCartById(String cartId) {
+    public void deleteGameEntry(String userId, String gameId) {
 
     }
 
