997857c2212dd45cf58f612a03b6bd5ab94b6afe
SergeyPodgorny <serkonpod@gmail.com>
2022-10-22 17:14:32 +0300

    working ot testing service methods

--- server/cart-service/src/test/java/com/fnd/games_store/cart/test/service/UpdateCartTest.java
+++ server/cart-service/src/test/java/com/fnd/games_store/cart/test/service/UpdateCartTest.java
@@ -1,5 +1,10 @@
 package com.fnd.games_store.cart.test.service;
 
+import com.fnd.games_store.cart.dto.CartRequestDTO;
+import com.fnd.games_store.cart.dto.CartResponseDTO;
+import com.fnd.games_store.cart.dto.GameResponseDTO;
+import com.fnd.games_store.cart.entity.Cart;
+import com.fnd.games_store.cart.entity.Game;
 import com.fnd.games_store.cart.repository.CartRepository;
 import com.fnd.games_store.cart.service.implementation.CartService;
 import com.fnd.games_store.cart.test.utilities.ServiceTestUtilities;
@@ -12,8 +17,13 @@ import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
 
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.mockito.Mockito.doReturn;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -29,10 +39,19 @@ public class UpdateCartTest extends ServiceTestUtilities {
     private CartService service;
 
 
+    protected String userId = "user_1";
+
+    protected Set<GameResponseDTO> testGameList = new HashSet<>();
+
+    protected CartRequestDTO testRequest = new CartRequestDTO();
+
+    protected CartResponseDTO testResponse = new CartResponseDTO();
+
+    protected Cart testCart = new Cart();
 
     @Test
     void testing() {
-        assertThat(service.updateCart(testRequestCart)).isNull();
+//        assertThat(service.updateCart(testRequestCart)).isNull();
     }
 
 
@@ -40,7 +59,58 @@ public class UpdateCartTest extends ServiceTestUtilities {
     void testSetup(){
 
 //        testCart.setUserId(userId);
-//        testCart.setGameId(gameId);
+//        testGameList.add(createTestGameData("1"));
+//        testCart.setGameData(testGameList);
+//
+//        when(repository.findById(userId).get()).thenReturn();
+
+
+
+    }
+
+
+//    public CartResponseDTO updateCart(CartRequestDTO incomingCartData) {
+//        Cart updatingCart = new Cart();
+//        updatingCart.setUserId(incomingCartData.getUserId());
+//        updatingCart.setGameData(incomingCartData.getGameData());
+//
+//        repository.save(updatingCart);
+//
+//        return new CartResponseDTO(incomingCartData);
+
+
+    CartRequestDTO createRequest(){
+
+        testRequest.setUserId(userId);
+
+        return testRequest;
+    }
+
+
+//    protected CartResponseDTO createTestRequest(){
+//
+//    }
+
+
+    protected GameResponseDTO createTestGameData(String differenceParameter){
+        GameResponseDTO game = new GameResponseDTO();
+        game.setGameId("id"+ differenceParameter);
+        game.setName("name" + differenceParameter);
+        game.setGenre("genre" + differenceParameter);
+        game.setReleaseDate("date" + differenceParameter);
+        game.setDeveloper("developer" + differenceParameter);
+        game.setPublisher("publisher" + differenceParameter);
+        game.setPlatform("platform" + differenceParameter);
+        game.setFeatures("features" + differenceParameter);
+        game.setPrice(BigDecimal.valueOf(1000));
+        game.setDiscount(BigDecimal.valueOf(200));
+        game.setDescription("description" + differenceParameter);
+        game.setBase64Image("image" + differenceParameter);
+        return game;
+    }
+
+
+//        testCart.setUserId(userId);
 //        testCart.setGameData(createTestGameEntity("1"));
 //
 //
@@ -51,7 +121,4 @@ public class UpdateCartTest extends ServiceTestUtilities {
 //        doReturn(testGameList).when(repository).updateCart(testResponseCart);
 //
 //        doReturn(null).when(repository).deleteGameEntry(userId,gameId);
-    }
-
-
 }
--- server/cart-service/src/test/java/com/fnd/games_store/cart/test/utilities/ServiceTestUtilities.java
+++ server/cart-service/src/test/java/com/fnd/games_store/cart/test/utilities/ServiceTestUtilities.java
@@ -4,21 +4,13 @@ import com.fnd.games_store.cart.dto.CartRequestDTO;
 import com.fnd.games_store.cart.dto.CartResponseDTO;
 import com.fnd.games_store.cart.dto.GameResponseDTO;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ServiceTestUtilities {
 
 
-    protected String userId = "user_1";
 
-    protected String gameId = "game_1";
-
-    protected List<GameResponseDTO> testGameList = new ArrayList<>();
-
-    protected CartRequestDTO testRequestCart = new CartRequestDTO();
-
-    protected CartResponseDTO testResponseCart = new CartResponseDTO();
 
 
     protected ServiceTestUtilities() {
