f2ed541d51297231e92cd8af6d85b7902b9c5de2
SergeyPodgorny <serkonpod@gmail.com>
2022-11-17 19:20:12 +0300

    refactored path to login service

--- server/cart-service/src/main/java/com/fnd/games_store/cart/rest/UserValidator.java
+++ server/cart-service/src/main/java/com/fnd/games_store/cart/rest/UserValidator.java
@@ -6,13 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletRequest;
 
-@FeignClient("login")
+@FeignClient("login-service")
 public interface UserValidator {
 
 
-    @RequestMapping(method = RequestMethod.POST, value = "localhost:8082/login/v1/validate", consumes = "application/json")
+    @RequestMapping(method = RequestMethod.POST, value = "/v1/validate", consumes = "application/json")
     ResponseEntity<ValidationResponseDTO> validateUser(String token);
 
 }
