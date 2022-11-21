16227f21c1a99cfa209b8031c60a67a7799e5692
SergeyPodgorny <serkonpod@gmail.com>
2022-11-21 17:33:04 +0300

    deployed first filter

--- server/games-service/src/main/java/com/fnd/games_store/games/configurations/WebSecurityConfiguration.java
+++ server/games-service/src/main/java/com/fnd/games_store/games/configurations/WebSecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.fnd.games_store.games.configurations;
 
+import com.fnd.games_store.games.filter.AuthorityValidationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,10 +12,20 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 public class WebSecurityConfiguration {
 
+
+    private final AuthorityValidationFilter authorityValidationFilter;
+    @Autowired
+    public WebSecurityConfiguration(AuthorityValidationFilter authorityValidationFilter) {
+        this.authorityValidationFilter = authorityValidationFilter;
+    }
+
+
     @Bean
     public SecurityFilterChain filter(HttpSecurity http) throws Exception {
 
         http.authorizeRequests().antMatchers("/v1/catalogue/all", "/h2/**").permitAll().anyRequest().authenticated();
+
+        http.addFilterBefore(authorityValidationFilter, UsernamePasswordAuthenticationFilter.class);
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
 
---
+++ server/games-service/src/main/java/com/fnd/games_store/games/filter/AuthorityValidationFilter.java
@@ -0,0 +1,27 @@
+package com.fnd.games_store.games.filter;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@Component
+@Slf4j
+public class AuthorityValidationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+
+        log.info("==============filtered===================");
+
+
+
+        filterChain.doFilter(request,response);
+
+    }
+}
