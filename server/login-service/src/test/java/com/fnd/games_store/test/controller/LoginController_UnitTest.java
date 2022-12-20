45f924250fc58ba7816d7892f0b834761fbd44a6
SergeyPodgorny <serkonpod@gmail.com>
2022-12-20 19:46:34 +0300

    old login controller unit test refactore and now working

--- server/login-service/src/test/java/com/fnd/games_store/test/controller/LoginController_UnitTest.java
+++ server/login-service/src/test/java/com/fnd/games_store/test/controller/LoginController_UnitTest.java
@@ -1,11 +1,14 @@
 package com.fnd.games_store.test.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fnd.games_store.login.LoginApplication;
+import com.fnd.games_store.login.dto.LoginRequestDTO;
 import com.fnd.games_store.login.dto.LoginResponseDTO;
 import com.fnd.games_store.login.service.implementation.LoginServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,14 +18,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = LoginApplication.class)
 @AutoConfigureMockMvc
-public class LoginControllerTest {
+public class LoginController_UnitTest {
 
 
     @Autowired
@@ -36,12 +38,12 @@ public class LoginControllerTest {
 
     private String password;
 
+    protected ObjectMapper objectMapper = new ObjectMapper();
 
-
-//    @Test
-    void controllerShouldReturnProperString() throws Exception {
-            MvcResult requestResult = this.mvc.perform(get("/v1/login")
-                            .content(new LoginResponseDTO("","token").getToken())
+    @Test
+    void login_ShouldReturnProperResponseObject() throws Exception {
+            MvcResult requestResult = this.mvc.perform(post("/v1/authorization")
+                            .content(jsonCartRequestDTO(createAppropriateGameRequestDTO(username,password)))
                             .contentType(MediaType.APPLICATION_JSON)
                             .accept(MediaType.APPLICATION_JSON)).
                             andExpect(status().isOk()).andReturn();
@@ -61,14 +63,23 @@ public class LoginControllerTest {
         username = "admin";
         password = "123";
         //TODO refactor test later
-//        when(loginService.login(username, password))
-//                .thenReturn(new LoginResponseDTO("","token").getToken());
+        when(loginService.login(username, password)).thenReturn(new LoginResponseDTO("","token"));
     }
 
 
+    private LoginRequestDTO createAppropriateGameRequestDTO(String username, String password){
+        return new LoginRequestDTO(username,password);
+    }
 
+    private String jsonCartRequestDTO(LoginRequestDTO loginRequestDTO) {
 
-
+        try {
+            return objectMapper.writeValueAsString(createAppropriateGameRequestDTO(loginRequestDTO.getUsername(),loginRequestDTO.getPassword()));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
 
 
 }
