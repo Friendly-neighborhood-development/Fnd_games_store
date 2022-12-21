88fa641125147ac5f6bd6f84a5ff7c477f21ff1a
SergeyPodgorny <serkonpod@gmail.com>
2022-12-21 19:41:26 +0300

    fixed issue with lombok version

--- server/games-service/src/main/java/com/fnd/games_store/games/filter/AdminAuthorityValidationFilter.java
+++ server/games-service/src/main/java/com/fnd/games_store/games/filter/AdminAuthorityValidationFilter.java
@@ -1,7 +1,9 @@
 package com.fnd.games_store.games.filter;
 
 
+import com.fnd.games_store.games.rest.AuthorityValidationClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -12,10 +14,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 @Slf4j
 public class AdminAuthorityValidationFilter extends OncePerRequestFilter {
+
+
+//    @Autowired
+//    private AuthorityValidationClient authorityValidationClient;
+
+    public AdminAuthorityValidationFilter() {
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        log.info("==============ADMIN===================");
+
+//        authorityValidationClient.validateUserWithAuthority(request.getHeader("authorization"));
+
+        log.info(request.getHeader("authorization"));
 
         filterChain.doFilter(request,response);
 
