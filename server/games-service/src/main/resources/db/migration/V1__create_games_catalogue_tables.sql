96a5b41c3adaaadd0524ae7fc062680f9e01524d
SergeyPodgorny <serkonpod@gmail.com>
2022-12-24 13:51:27 +0300

    Platform entity successfully integrated

--- server/games-service/src/main/resources/db/migration/V1__create_games_catalogue_tables.sql
+++ server/games-service/src/main/resources/db/migration/V1__create_games_catalogue_tables.sql
@@ -10,7 +10,9 @@ CREATE TABLE IF NOT EXISTS developers
 (developer_id varchar(255) PRIMARY KEY UNIQUE NOT NULL,
 developer_name varchar(255) NOT NULL);
 
-
+CREATE TABLE IF NOT EXISTS platforms
+(platform_id varchar(255) PRIMARY KEY UNIQUE NOT NULL,
+platform_name varchar(255) NOT NULL);
 
 
 
@@ -20,7 +22,6 @@ name varchar(255) UNIQUE NOT NULL,
 release_date varchar(50) NOT NULL,
 developer_id varchar(255) NOT NULL,
 publisher_id varchar(255) NOT NULL,
-platform varchar(255) NOT NULL,
 features varchar(255) NOT NULL,
 price numeric(10,2) NOT NULL,
 discount numeric(10,2) NOT NULL,
@@ -43,4 +44,9 @@ FOREIGN KEY(game_id) REFERENCES games(game_id),
 FOREIGN KEY(genre_id) REFERENCES genres(genre_id));
 
 
+CREATE TABLE IF NOT EXISTS game_platform
+(game_id varchar(255),
+platform_id varchar(255),
 
+FOREIGN KEY(game_id) REFERENCES games(game_id),
+FOREIGN KEY(platform_id) REFERENCES platforms(platform_id));
