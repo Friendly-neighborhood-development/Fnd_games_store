37f12ef8241e2a9963aced61615c2755f27235b2
Andrew-Sem <avvyayvayva@gmail.com>
2022-12-24 22:54:28 +0300

    update footer UI and change text inside
    update bg, border and change text to more representative: Copyright ... FND

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,24 +4,8 @@ import {PuzzlePieceIcon} from "@heroicons/react/20/solid";
 
 const Footer = () => {
     return (
-        <footer className={"bg-white dark:bg-slate-800 mt-4 w-full py-4 lg:py-6"}>
-
-            {/*Тут либо выравнивание по центру, либо по бокам всё раскидать, но надо ещё наполнения побольше*/}
-
-            {/*<div className={"container mx-auto flex flex-col items-center space-y-4"}>*/}
-            {/*    <div className={"flex items-center space-x-2"}>*/}
-            {/*        <PuzzlePieceIcon className={"text-blue-600 dark:text-sky-500 w-8 h-8"}/>*/}
-            {/*        <span*/}
-            {/*            className={"text-gray-600 dark:text-gray-400"}>&#169; 2022 Friendly neighborhood development</span>*/}
-            {/*    </div>*/}
-            {/*    <div className={"flex items-center space-x-2"}>*/}
-            {/*        <span className={"text-sm"}>Follow us on Github</span>*/}
-            {/*        <a href={"https://github.com/Friendly-neighborhood-development"}>*/}
-            {/*            <img src={images.githubLogo} alt={"github logo"} className={"w-8 h-8"}/>*/}
-            {/*        </a>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-            <div className={"container mx-auto flex items-center justify-between flex-col lg:flex-row-reverse"}>
+        <footer className={"bg-white dark:bg-slate-900 mt-4 w-full py-4 lg:py-6 border-t border-slate-500/30"}>
+            <div className={"mx-auto flex items-center justify-between flex-col lg:flex-row-reverse"}>
                 <div className={"flex items-center space-x-2"}>
                     <span className={"text-sm"}>Follow us on Github</span>
                     <span className={"bg-white rounded-full h-8 w-8 flex justify-center items-center"}>
@@ -31,8 +15,7 @@ const Footer = () => {
                     </span>
                 </div>
                 <div className={"flex items-center space-x-2"}>
-                    <PuzzlePieceIcon className={"text-blue-600 dark:text-sky-500 w-8 h-8"}/>
-                    <span className={"text-gray-600 dark:text-gray-400"}>&#169; 2022 Friendly neighborhood development</span>
+                    <span className={"text-gray-600 dark:text-gray-400"}>Copyright &#169; 2022 FND</span>
                 </div>
             </div>
         </footer>
