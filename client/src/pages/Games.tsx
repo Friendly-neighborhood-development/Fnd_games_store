7433faaf2f0b98bef4431f1871a5fa51719b2a9e
SergeyPodgorny <serkonpod@gmail.com>
2022-12-28 18:56:50 +0300

    solving rebase conflict with dev branch

--- client/src/pages/Games.tsx
+++ client/src/pages/Games.tsx
@@ -1,22 +1,12 @@
-import React, {useEffect} from 'react';
-import GamesCategory from "../components/GamesCategory";
+import React from 'react';
+import GamesList from "../components/GamesList";
 import MainLayout from "../components/layouts/MainLayout";
-import {useAppDispatch, useAppSelector} from "../hooks/redux";
-import {fetchGames} from "../store/actions/gameAction";
 
 const Games = () => {
-    const {games, isLoading, error} = useAppSelector(state => state.game)
-    const dispatch = useAppDispatch()
-    useEffect(() => {
-        dispatch(fetchGames())
-    }, [])
-
     return (
         <MainLayout>
             <div className={"w-full flex flex-col"}>
-                <GamesCategory title={"Популярные"} type={"popular"} games={games} isLoading={isLoading} error={error}/>
-                <GamesCategory title={"Распродажа"} type={"sale"} games={games} isLoading={isLoading} error={error}/>
-                <GamesCategory title={"Новинки"} type={"new"} games={games} isLoading={isLoading} error={error}/>
+                <GamesList/>
             </div>
         </MainLayout>
     );
