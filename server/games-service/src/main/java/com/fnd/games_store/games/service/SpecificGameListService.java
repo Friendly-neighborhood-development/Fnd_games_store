559c5825c4c856e4aabc7201340a75abd72c250d
SergeyPodgorny <serkonpod@gmail.com>
2022-12-29 18:42:31 +0300

    service interfaces and their implementations refactored

--- server/games-service/src/main/java/com/fnd/games_store/games/service/SpecificGameListService.java
+++ server/games-service/src/main/java/com/fnd/games_store/games/service/SpecificGameListService.java
@@ -1,12 +1,11 @@
 package com.fnd.games_store.games.service;
 
 import com.fnd.games_store.games.dto.GameResponseDTO;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
-public interface GamePagingAndSortingService {
+public interface SpecificGameListService {
 
 
     List<GameResponseDTO> getSpecifiedGameList(Integer pageNumber, Integer gamesOnPage, Sort sortBy);
