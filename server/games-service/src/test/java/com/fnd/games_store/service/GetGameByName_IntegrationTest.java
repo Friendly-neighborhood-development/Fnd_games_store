bc1fc20f1ecb296ffd84b36d8260b954fc0a7138
SergeyPodgorny <serkonpod@gmail.com>
2022-12-29 17:05:13 +0300

    refactoring test

--- server/games-service/src/test/java/com/fnd/games_store/service/GetGameByName_IntegrationTest.java
+++ server/games-service/src/test/java/com/fnd/games_store/service/GetGameByName_IntegrationTest.java
@@ -1,46 +1,53 @@
 package com.fnd.games_store.service;
 
+import com.fnd.games_store.games.GamesApplication;
 import com.fnd.games_store.games.dto.GameResponseDTO;
+import com.fnd.games_store.games.entity.Game;
 import com.fnd.games_store.games.repository.GameRepository;
-import org.junit.Test;
+import com.fnd.games_store.games.service.GamePagingAndSortingService;
+import com.fnd.games_store.games.service.implementation.GamePagingAndSortingServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.test.context.BootstrapWith;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import static org.assertj.core.api.Assertions.*;
 
-@SpringBootTest
-@DataJpaTest
-@BootstrapWith(SpringBootTestContextBootstrapper.class)
+
+@SpringBootTest(classes = GamesApplication.class)
+@Slf4j
 public class GetGameByName_IntegrationTest {
 
 
-
+    @Autowired
     private GameRepository repository;
 
-    private List<GameResponseDTO> referenceList;
+    @Autowired
+    private GamePagingAndSortingServiceImpl testableService;
 
-    private Integer page = 0;
+    private GameResponseDTO referenceGame;
 
-    private Integer pageSize = 2;
-
-    private String sortField = "name";
-    private Sort sortBy = Sort.by(sortField);
+    private String referenceGameName = "DOOM Eternal";
 
 
     @Test
     void getGameByName_ShouldReturnProperGameResponseDTO(){
-
+        assertThat(referenceGame).isEqualTo(createAppropriateGameResponseDTO());
     }
 
     @BeforeEach
     void testSetup(){
-        referenceList = repository.findAll(PageRequest.of(page,pageSize, sortBy)).stream().map(GameResponseDTO::new).collect(Collectors.toList());
+
+//        Game gameFromDb = repository.getGameByName(referenceGameName);
+//        log.info(gameFromDb.getName());
+        referenceGame = new GameResponseDTO(repository.getGameByName(referenceGameName));
+//        referenceGame.setId("1");
+//        referenceGame.setBase64Image("");
+//        referenceGame.setDescription("");
     }
 
 
@@ -48,9 +55,31 @@ public class GetGameByName_IntegrationTest {
 
     private GameResponseDTO createAppropriateGameResponseDTO() {
         GameResponseDTO returnableGame = new GameResponseDTO();
+        returnableGame.setId("1");
         returnableGame.setName("Doom Eternal");
+        returnableGame.setReleaseDate("2019");
+        returnableGame.setDeveloper(null);
+        returnableGame.setPublisher(null);
 
+
+
+//        '1','DOOM Eternal', '2019', '1','1', 1500.64, 15.2,
         return returnableGame;
     }
+
+    //    private List<GameResponseDTO> referenceList;
+//
+//    private Integer page = 0;
+//
+//    private Integer pageSize = 2;
+//
+//    private String sortField = "name";
+//    private Sort sortBy = Sort.by(sortField);
+
+    //        referenceList = repository.findAll(PageRequest.of(page,pageSize, sortBy)).stream().map(GameResponseDTO::new).collect(Collectors.toList());
+
+
+
+
 
 }
