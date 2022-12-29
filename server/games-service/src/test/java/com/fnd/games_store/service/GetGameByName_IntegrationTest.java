d301241742d61af0d7e2c1d252880bf49593969b
SergeyPodgorny <serkonpod@gmail.com>
2022-12-29 16:30:46 +0300

    test setup deployed

--- server/games-service/src/test/java/com/fnd/games_store/service/GetGameByName_IntegrationTest.java
+++ server/games-service/src/test/java/com/fnd/games_store/service/GetGameByName_IntegrationTest.java
@@ -3,10 +3,16 @@ package com.fnd.games_store.service;
 import com.fnd.games_store.games.dto.GameResponseDTO;
 import com.fnd.games_store.games.repository.GameRepository;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.BootstrapWith;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 @DataJpaTest
@@ -17,12 +23,27 @@ public class GetGameByName_IntegrationTest {
 
     private GameRepository repository;
 
+    private List<GameResponseDTO> referenceList;
+
+    private Integer page = 0;
+
+    private Integer pageSize = 2;
+
+    private String sortField = "name";
+    private Sort sortBy = Sort.by(sortField);
 
 
     @Test
     void getGameByName_ShouldReturnProperGameResponseDTO(){
 
     }
+
+    @BeforeEach
+    void testSetup(){
+        referenceList = repository.findAll(PageRequest.of(page,pageSize, sortBy)).stream().map(GameResponseDTO::new).collect(Collectors.toList());
+    }
+
+
 
 
     private GameResponseDTO createAppropriateGameResponseDTO() {
