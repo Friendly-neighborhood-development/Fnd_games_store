4e402d665d6eafc994a83cc737d462fe5de192d2
Andrew-Sem <avvyayvayva@gmail.com>
2022-12-30 15:44:09 +0300

    added games filter to large screen, refactor sidebar

--- client/src/components/GamesList.tsx
+++ client/src/components/GamesList.tsx
@@ -3,6 +3,8 @@ import GameCard from "./GameCard";
 import {useAppDispatch, useAppSelector} from "../hooks/redux";
 import {fetchGames} from "../store/actions/gamesAction";
 import {useNavigate} from "react-router-dom";
+import Sidebar from "./Sidebar";
+import {GamesFilter} from "./GamesFilter";
 
 
 const GamesList: FC = memo(() => {
@@ -18,22 +20,29 @@ const GamesList: FC = memo(() => {
 
     return (
         <section className={"w-full my-4"}>
-            <div className={""}>
-                <div className={"text-xl mb-2"}>Games</div>
-                <div className={"lg:hidden"}>filter</div>
-            </div>
-            {error
-                ? <h1>Ошибка: {error}</h1>
-                : <div
-                    className={"flex space-x-2 w-full overflow-x-auto lg:flex-none lg:space-x-0 lg:overflow-x-visible lg:grid lg:gap-x-4 lg:grid-cols-3 lg:gap-y-4 xl:grid-cols-4"}>
-                    {games.map(game =>
-                        <GameCard
-                            {...game}
-                            key={game.id}
-                            skeleton={loading === "succeeded"}
-                        />)}
-                </div>
+            {games.length !== 0 &&
+                <Sidebar>
+                    <GamesFilter/>
+                </Sidebar>
             }
+            <div className={"lg:ml-64"}>
+                <div className={""}>
+                    <div className={"text-xl mb-2"}>Games</div>
+                    <div className={"lg:hidden"}>filter</div>
+                </div>
+                {error
+                    ? <h1>Ошибка: {error}</h1>
+                    : <div
+                        className={"flex space-x-2 w-full overflow-x-auto lg:flex-none lg:space-x-0 lg:overflow-x-visible lg:grid lg:gap-x-4 lg:grid-cols-3 lg:gap-y-4 xl:grid-cols-4"}>
+                        {games.map(game =>
+                            <GameCard
+                                {...game}
+                                key={game.id}
+                                skeleton={loading === "succeeded"}
+                            />)}
+                    </div>
+                }
+            </div>
         </section>
     );
 })
