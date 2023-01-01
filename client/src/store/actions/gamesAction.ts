eebf2a0d34d7734bf8a2e193c2a00babc230bef3
Andrew-Sem <avvyayvayva@gmail.com>
2023-01-01 15:22:23 +0300

    refactored gamesAction

--- client/src/store/actions/gamesAction.ts
+++ client/src/store/actions/gamesAction.ts
@@ -1,8 +1,16 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 import {GameService} from "../../services/GameService";
 
+interface fetchGamesProps {
+    page: number,
+    pageSize: number,
+    sortField: string,
+    ascOrder: boolean
+}
 
-export const fetchGames = createAsyncThunk("games/catalogue", async({page, pageSize, sortField, ascOrder}:{page:number, pageSize: number, sortField: string, ascOrder: boolean})=>{
-    const res = await GameService.fetchGames(page, pageSize, sortField, ascOrder)
-    return res.data
-})
+export const fetchGames = createAsyncThunk("games/catalogue",
+    async ({page, pageSize, sortField, ascOrder}: fetchGamesProps) => {
+        const res = await GameService.fetchGames(page, pageSize, sortField, ascOrder)
+        return res.data
+    })
+
