f75c603792d645491f1a263541f6a606a5406f02
SergeyPodgorny <serkonpod@gmail.com>
2023-01-01 06:58:32 +0300

    added equals(), hashcode() and toString() to GameResponseDTO

--- server/cart-service/src/main/java/com/fnd/games_store/cart/dto/GameResponseDTO.java
+++ server/cart-service/src/main/java/com/fnd/games_store/cart/dto/GameResponseDTO.java
@@ -36,15 +36,29 @@ public class GameResponseDTO {
 
     @Override
     public String toString() {
-        return "{" +
-                "id=" + id +
-                ", name=" + name +
-                ", releaseDate=" + releaseDate +
+        return "GameResponseDTO{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                ", gameUrl='" + gameUrl + '\'' +
                 ", price=" + price +
                 ", discount=" + discount +
-                ", description=" + description +
-                ", base64Image=" + base64Image +
-                "}";
+                ", description='" + description + '\'' +
+                ", base64Image='" + base64Image + '\'' +
+                '}';
     }
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameResponseDTO that = (GameResponseDTO) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(releaseDate, that.releaseDate) && Objects.equals(gameUrl, that.gameUrl) && Objects.equals(price, that.price) && Objects.equals(discount, that.discount) && Objects.equals(description, that.description) && Objects.equals(base64Image, that.base64Image);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, releaseDate, gameUrl, price, discount, description, base64Image);
+    }
 }
