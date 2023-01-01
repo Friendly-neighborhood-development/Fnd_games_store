4c5dfd47b90f009cc93bbd13e949b9eda19f1998
Andrew-Sem <avvyayvayva@gmail.com>
2023-01-01 17:14:38 +0300

    decomposed games filter constants

---
+++ client/src/constants/filter.js
@@ -0,0 +1,26 @@
+export const fields = [
+    {id: 1, title: 'name'},
+    {id: 2, title: 'discount'},
+    {id: 3, title: 'releaseDate'},
+    {id: 4, title: 'platform'},
+    {id: 5, title: 'price'},
+]
+
+export const pages = [
+    {id: 1, title: "1"},
+    {id: 2, title: "2"},
+    {id: 3, title: "3"},
+    {id: 4, title: "4"},
+    {id: 5, title: "5"},
+]
+
+export const orders = [
+    {id: 1, title: "direct"},
+    {id: 2, title: "reverse"},
+]
+
+export const pageSizes = [
+    {id: 1, title: 5},
+    {id: 2, title: 10},
+    {id: 3, title: 20},
+]
