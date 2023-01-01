3fd0fc8c56864c4bef363a5251a67750a3f7311a
Andrew-Sem <avvyayvayva@gmail.com>
2023-01-01 15:48:55 +0300

    update game model

--- client/src/models/IGame.ts
+++ client/src/models/IGame.ts
@@ -1,14 +1,39 @@
+interface IGenre {
+    id: string,
+    name: string
+}
+
+interface IDeveloper {
+    id: string,
+    name: string
+}
+
+interface IPlatform {
+    id: string
+    name: string
+}
+
+interface IFeature {
+    id: string,
+    name: string
+}
+
+interface IPublisher {
+    id: string,
+    name: string
+}
+
 export interface IGame {
     id: number
     name: string
     base64Image: string
     description: string
-    developer: string
+    developer: IDeveloper
     discount: number
-    features: string
-    genre: string
-    platform: string
+    features: Array<IFeature>
+    genre: Array<IGenre>
+    platform: Array<IPlatform>
     price: number
-    publisher: string
+    publisher: IPublisher
     releaseDate: string
 }
