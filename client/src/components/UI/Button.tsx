7d07fa6717ff3ad3e3ba8f18d0366eee52337fee
Andrew-Sem <avvyayvayva@gmail.com>
2023-01-02 15:53:50 +0300

    delete default text color for Button

--- client/src/components/UI/Button.tsx
+++ client/src/components/UI/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 }
 
 const Button: FC<ButtonProps> = ({className, children, ...props}) => {
-    const rootClasses = "px-6 py-2 text-sm self-center cursor-pointer rounded dark:text-slate-200 " + className
+    const rootClasses = "px-6 py-2 text-sm self-center cursor-pointer rounded " + className
     return (
         <button className={rootClasses} {...props}>
             {children}
