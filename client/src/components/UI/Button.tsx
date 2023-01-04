1c1f477c5c3688f210028407fcb47125a98ca2b5
Andrew-Sem <avvyayvayva@gmail.com>
2023-01-04 17:10:32 +0300

    updated button style for smallscreen
    now padding-y more larger

--- client/src/components/UI/Button.tsx
+++ client/src/components/UI/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 }
 
 const Button: FC<ButtonProps> = ({className, children, ...props}) => {
-    const rootClasses = "px-6 py-2 text-sm self-center cursor-pointer rounded capitalize " + className
+    const rootClasses = "px-6 lg:py-2 py-3 text-sm self-center cursor-pointer rounded capitalize " + className
     return (
         <button className={rootClasses} {...props}>
             {children}
