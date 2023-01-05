3d36f786e133abda6d814443599ca1f6a07cb901
SergeyPodgorny <serkonpod@gmail.com>
2023-01-05 08:37:10 +0300

    getGameByName() service method succesfully tested

--- server/games-service/src/test/java/com/fnd/games_store/service/GetGameByName_IntegrationTest.java
+++ server/games-service/src/test/java/com/fnd/games_store/service/GetGameByName_IntegrationTest.java
@@ -2,71 +2,59 @@ package com.fnd.games_store.service;
 
 import com.fnd.games_store.games.GamesApplication;
 import com.fnd.games_store.games.dto.game.GameResponseDTO;
+import com.fnd.games_store.games.repository.GameRepository;
 import com.fnd.games_store.games.service.SpecificGameService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest(classes = GamesApplication.class)
 @Slf4j
+@Transactional
 public class GetGameByName_IntegrationTest {
 
 
     @Autowired
     private SpecificGameService service;
 
+    @Autowired
+    private GameRepository repository;
+
     private GameResponseDTO serviceGameEntity;
 
     private GameResponseDTO controlGameEntity;
 
-    private String controlGameName = "Grand Theft Auto V";
+    private String controlGameName = "DOOM Eternal";
 
 
     @Test
     void getGameByName_ShouldReturnProperGameResponseDTO(){
-        assertThat(service.getGameByName(controlGameName)).isEqualTo(controlGameEntity);
+        assertThat(serviceGameEntity).isEqualTo(controlGameEntity);
     }
 
 
     @BeforeEach
     void testSetup(){
 
+        controlGameEntity = createAppropriateGameResponseDTO();
 
-        this.controlGameEntity = createAppropriateGameResponseDTO();
-
+        serviceGameEntity = service.getGameByName(controlGameName);
 
     }
 
     private GameResponseDTO createAppropriateGameResponseDTO() {
-        GameResponseDTO returnableGame = new GameResponseDTO();
-        returnableGame.setId("1");
-        returnableGame.setName("Doom Eternal");
-        returnableGame.setReleaseDate("2019");
-        returnableGame.setDeveloper(null);
-        returnableGame.setPublisher(null);
 
-
+        GameResponseDTO returnableGame = new GameResponseDTO(repository.getGameByName(controlGameName).get());
 
         return returnableGame;
     }
-
-    //    private List<GameResponseDTO> referenceList;
-//
-//    private Integer page = 0;
-//
-//    private Integer pageSize = 2;
-//
-//    private String sortField = "name";
-//    private Sort sortBy = Sort.by(sortField);
-
-    //        referenceList = repository.findAll(PageRequest.of(page,pageSize, sortBy)).stream().map(GameResponseDTO::new).collect(Collectors.toList());
-
-
-
-
 
 }
