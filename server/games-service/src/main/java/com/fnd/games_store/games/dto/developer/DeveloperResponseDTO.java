2949dbd16cd1953fc5fe9a99fb234b29e08fb998
SergeyPodgorny <serkonpod@gmail.com>
2023-01-05 07:45:56 +0300

    developer response dto correctly inserted into game response dto

--- server/games-service/src/main/java/com/fnd/games_store/games/dto/developer/DeveloperResponseDTO.java
+++ server/games-service/src/main/java/com/fnd/games_store/games/dto/developer/DeveloperResponseDTO.java
@@ -1,20 +1,30 @@
 package com.fnd.games_store.games.dto.developer;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fnd.games_store.games.entity.Game;
-import com.sun.istack.NotNull;
-import org.hibernate.annotations.GenericGenerator;
+import com.fnd.games_store.games.entity.Developer;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-import javax.persistence.*;
-import java.util.List;
-
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public class DeveloperResponseDTO {
 
 
     private String id;
 
-
     private String name;
 
 
+    public DeveloperResponseDTO(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public DeveloperResponseDTO(Developer developer) {
+        this.id = developer.getId();
+        this.name = developer.getName();
+    }
 }
