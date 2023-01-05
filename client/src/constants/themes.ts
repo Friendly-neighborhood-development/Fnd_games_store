0446e91de5c4969aa93a6d2251348ce57d52324d
Andrew-Sem <avvyayvayva@gmail.com>
2023-01-05 21:45:26 +0300

    switch themes ids to string

--- client/src/constants/themes.ts
+++ client/src/constants/themes.ts
@@ -2,7 +2,7 @@ import {ITheme} from "../models/ITheme";
 import {ComputerDesktopIcon, MoonIcon, SunIcon} from "@heroicons/react/24/outline";
 
 export const themes: Array<ITheme> = [
-    {id: 1, title: 'light', Icon: SunIcon},
-    {id: 2, title: 'dark', Icon: MoonIcon},
-    {id: 3, title: 'system', Icon: ComputerDesktopIcon},
+    {id: "1", title: 'light', Icon: SunIcon},
+    {id: "2", title: 'dark', Icon: MoonIcon},
+    {id: "3", title: 'system', Icon: ComputerDesktopIcon},
 ]
