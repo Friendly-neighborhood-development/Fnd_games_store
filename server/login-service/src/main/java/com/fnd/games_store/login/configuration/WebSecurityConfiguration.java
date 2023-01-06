248340afb75cbb13006c2ad8ca1d840f05f0543d
SergeyPodgorny <serkonpod@gmail.com>
2023-01-06 12:31:42 +0300

    added new test case: testing UserRegistrationController

--- server/login-service/src/main/java/com/fnd/games_store/login/configuration/WebSecurityConfiguration.java
+++ server/login-service/src/main/java/com/fnd/games_store/login/configuration/WebSecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.fnd.games_store.login.exception.JwtAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -45,6 +46,7 @@ public class WebSecurityConfiguration {
     }
 
     @Bean
+    @Profile("standalone")
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring()
                 .antMatchers("/h2/*");
---
+++ server/login-service/src/test/java/com/fnd/games_store/test/controller/UserRegistrationTest.java
@@ -0,0 +1,29 @@
+package com.fnd.games_store.test.controller;
+
+
+import com.fnd.games_store.login.LoginApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = LoginApplication.class)
+@AutoConfigureMockMvc
+public class UserRegistrationTest {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
