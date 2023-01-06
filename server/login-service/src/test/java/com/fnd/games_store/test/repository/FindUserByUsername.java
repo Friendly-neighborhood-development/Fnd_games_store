4dc19f93616460ee580cee25d6613d43fdf3e027
SergeyPodgorny <serkonpod@gmail.com>
2023-01-06 07:00:01 +0300

    FindUSerByUsername renamed

--- server/login-service/src/test/java/com/fnd/games_store/test/repository/FindUserByUsername.java
+++ server/login-service/src/test/java/com/fnd/games_store/test/repository/FindUserByUsername.java
@@ -3,7 +3,6 @@ package com.fnd.games_store.test.repository;
 
 import com.fnd.games_store.login.LoginApplication;
 import com.fnd.games_store.login.entity.Account;
-import com.fnd.games_store.login.entity.Authority;
 import com.fnd.games_store.login.repository.AccountRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,12 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.test.context.BootstrapWith;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 
 import static org.assertj.core.api.Assertions.*;
@@ -24,7 +18,7 @@ import static org.assertj.core.api.Assertions.*;
 @DataJpaTest
 @BootstrapWith(SpringBootTestContextBootstrapper.class)
 @SpringBootTest(classes = LoginApplication.class)
-public class FindUserByUsername_IntegrationTest extends RepositoryTestCommons{
+public class FindUserByUsername extends RepositoryTestCommons{
 
 
     @Autowired
