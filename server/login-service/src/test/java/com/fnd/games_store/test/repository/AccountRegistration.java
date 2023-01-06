56637511602f7aef997cf2512d88576e257c325f
SergeyPodgorny <serkonpod@gmail.com>
2023-01-06 07:10:45 +0300

    added utility methods and new filed to requestdto

--- server/login-service/src/test/java/com/fnd/games_store/test/repository/AccountRegistration.java
+++ server/login-service/src/test/java/com/fnd/games_store/test/repository/AccountRegistration.java
@@ -2,6 +2,11 @@ package com.fnd.games_store.test.repository;
 
 
 import com.fnd.games_store.login.LoginApplication;
+import com.fnd.games_store.login.dto.AccountRequestDTO;
+import com.fnd.games_store.login.dto.AccountResponseDTO;
+import com.fnd.games_store.login.repository.AccountRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
@@ -13,6 +18,31 @@ import org.springframework.test.context.BootstrapWith;
 public class AccountRegistration extends RepositoryTestCommons{
 
 
+    @Autowired
+    private AccountRepository repository;
+
+
+    private String username = "Alfonso";
+
+    private String password = "original_password_123";
+
+    private String email = "alfonso@email.com";
+
+    private AccountRequestDTO newAccountData;
+
+
+
+    @Test
+    void register_ShouldSaveNewAccountEntityToDB(){
+
+    }
+
+
+
+    private AccountRequestDTO createAppropriateNewAccountDTO(String username, String password, String email){
+        AccountRequestDTO savingAccount = new AccountRequestDTO(username,password,email);
+        return savingAccount;
+    }
 
 
 
