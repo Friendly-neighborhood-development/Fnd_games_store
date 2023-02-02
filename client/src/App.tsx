753d312c3699933331e51695df65a96bad499c02
Andrew-Sem <avvyayvayva@gmail.com>
2023-02-02 11:21:35 +0300

    fix dispatching check auth on every rerender

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,19 @@
-import React, {FC, useEffect} from 'react';
-import {BrowserRouter as Router} from "react-router-dom";
-import {AppRouter} from "./components/AppRouter";
-import {useAppDispatch} from "./hooks/redux";
-import {checkAuth} from "./store/reducers/authSlice";
+import React, { FC, useEffect } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AppRouter } from './components/AppRouter';
+import { useAppDispatch } from './hooks/redux';
+import { checkAuth } from './store/reducers/authSlice';
 
 const App: FC = () => {
-    const dispatch = useAppDispatch()
+    const dispatch = useAppDispatch();
     useEffect(() => {
-        dispatch(checkAuth())
-    })
+        dispatch(checkAuth());
+    }, []);
     return (
         <Router>
-            <AppRouter/>
+            <AppRouter />
         </Router>
     );
-}
+};
 
 export default App;
