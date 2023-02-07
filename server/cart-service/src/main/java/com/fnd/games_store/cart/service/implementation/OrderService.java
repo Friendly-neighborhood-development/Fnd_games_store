fcfceb0f36b7f87709d0dead5559cc6353a07f14
SergeyPodgorny <serkonpod@gmail.com>
2023-02-07 17:36:35 +0300

    cart flush functionality deployed

--- server/cart-service/src/main/java/com/fnd/games_store/cart/service/implementation/OrderService.java
+++ server/cart-service/src/main/java/com/fnd/games_store/cart/service/implementation/OrderService.java
@@ -1,4 +1,38 @@
 package com.fnd.games_store.cart.service.implementation;
 
-public class OrderService {
+import com.fnd.games_store.cart.dto.CartResponseDTO;
+import com.fnd.games_store.cart.entity.Cart;
+import com.fnd.games_store.cart.repository.CartRepository;
+import com.fnd.games_store.cart.service.OrderProcessingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class OrderService implements OrderProcessingService {
+
+
+    private final CartRepository repository;
+
+    @Autowired
+    public OrderService(CartRepository repository) {
+        this.repository = repository;
+    }
+
+
+    @Override
+    public CartResponseDTO purchaseGames(String userId) {
+
+        Optional<Cart> flushingCartData = repository.findById(userId);
+
+        if (flushingCartData.isPresent()){
+            repository.deleteById(userId);
+        } else {
+
+        }
+
+
+        return null;
+    }
 }
