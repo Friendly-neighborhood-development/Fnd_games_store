d849318c2556dff6aec059be44d4d16d17b25590
SergeyPodgorny <serkonpod@gmail.com>
2023-02-09 17:42:16 +0300

    deloyed new data transfer object, which transfer data to orders service via kafka

---
+++ server/cart-service/src/main/java/com/fnd/games_store/cart/dto/MessageToOrderServiceDTO.java
@@ -0,0 +1,23 @@
+package com.fnd.games_store.cart.dto;
+
+import com.fnd.games_store.cart.entity.Game;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class MessageToOrderServiceDTO {
+
+
+    private String userId;
+
+    private List<Game> gameData;
+
+
+
+}
