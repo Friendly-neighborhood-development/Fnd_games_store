8d7de45b530ce37ee71c60fe69333c4c31d574cf
SergeyPodgorny <serkonpod@gmail.com>
2023-02-09 16:57:00 +0300

    added test case for Ordercontroller

---
+++ server/cart-service/src/test/java/com/fnd/games_store/cart/test/order_controller_integration_test/OrderController_ProcessOrderTest.java
@@ -0,0 +1,30 @@
+package com.fnd.games_store.cart.test.order_controller_integration_test;
+
+
+import com.fnd.games_store.cart.CartApplication;
+import com.fnd.games_store.cart.rest.UserValidationClient;
+import com.fnd.games_store.cart.service.OrderProcessingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(classes = CartApplication.class)
+public class OrderController_ProcessOrderTest {
+    
+    
+    
+    @MockBean
+    protected OrderProcessingService service;
+
+
+    @Autowired
+    protected MockMvc mvc;
+
+    @MockBean
+    protected UserValidationClient userValidationClient;
+    
+    
+    
+    
+}
