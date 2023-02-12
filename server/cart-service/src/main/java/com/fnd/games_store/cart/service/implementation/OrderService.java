cb9389460b912f96c9adb03e103964e47fc9f051
SergeyPodgorny <serkonpod@gmail.com>
2023-02-12 07:26:00 +0300

    message sending functionality deployed in cart-service

--- server/cart-service/src/main/java/com/fnd/games_store/cart/service/implementation/OrderService.java
+++ server/cart-service/src/main/java/com/fnd/games_store/cart/service/implementation/OrderService.java
@@ -1,24 +1,31 @@
 package com.fnd.games_store.cart.service.implementation;
 
 import com.fnd.games_store.cart.dto.CartResponseDTO;
+import com.fnd.games_store.cart.dto.OrderDTO;
 import com.fnd.games_store.cart.entity.Cart;
 import com.fnd.games_store.cart.exception.CartNotFoundException;
 import com.fnd.games_store.cart.repository.CartRepository;
 import com.fnd.games_store.cart.service.OrderProcessingService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class OrderService implements OrderProcessingService {
 
 
     private final CartRepository repository;
 
+    private final KafkaTemplate<String, OrderDTO> kafkaTemplate;
+
     @Autowired
-    public OrderService(CartRepository repository) {
+    public OrderService(CartRepository repository, KafkaTemplate<String, OrderDTO> kafkaTemplate) {
         this.repository = repository;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
 
@@ -27,6 +34,10 @@ public class OrderService implements OrderProcessingService {
 
 
         Cart erasedCartData = eraseCart(userId).get();
+
+        OrderDTO messageToOrdersService = new OrderDTO(erasedCartData);
+
+        sendMessage("order-stream", messageToOrdersService);
 
 
         return new CartResponseDTO(erasedCartData);
@@ -48,5 +59,8 @@ public class OrderService implements OrderProcessingService {
 
     }
 
-
+    private void sendMessage(String topicName, OrderDTO orderData) {
+        kafkaTemplate.send(topicName, orderData);
+        log.info(topicName + " has been sent");
+    }
 }
