b737b876b6778e64b58c9ce2fca1c00b32b5569e
SergeyPodgorny <serkonpod@gmail.com>
2023-02-12 09:03:54 +0300

    moving to JsonSerializer from custom Serializer in Cart-service

--- server/cart-service/src/main/java/com/fnd/games_store/cart/configuration/KafkaConfiguration.java
+++ server/cart-service/src/main/java/com/fnd/games_store/cart/configuration/KafkaConfiguration.java
@@ -3,8 +3,8 @@ package com.fnd.games_store.cart.configuration;
 import java.util.HashMap;
 import java.util.Map;
 
+
 import com.fnd.games_store.cart.dto.OrderDTO;
-import com.fnd.games_store.cart.serializer.OrderDTOSerializer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +13,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
 @Configuration
 public class KafkaConfiguration {
 
@@ -26,7 +28,7 @@ public class KafkaConfiguration {
         Map<String,Object> kafkaConfig = new HashMap<>();
         kafkaConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         kafkaConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        kafkaConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, OrderDTOSerializer.class);
+        kafkaConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(kafkaConfig);
     }
 
