8be7f300e3fe15a96afda2b058162542bf323c5e
SergeyPodgorny <serkonpod@gmail.com>
2023-02-12 07:17:08 +0300

    basic OrderMessageListenr implementation for receiving kafka messages deployed

---
+++ server/orders-service/src/main/java/com/fnd/games_store/orders/service/implementation/OrderReceiverService.java
@@ -0,0 +1,24 @@
+package com.fnd.games_store.orders.service.implementation;
+
+import com.fnd.games_store.orders.dto.OrderDTO;
+import com.fnd.games_store.orders.service.OrderMessageListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class OrderReceiverService implements OrderMessageListener {
+
+
+    @Value("${variables.kafka.group}")
+    private String customGroupId;
+
+
+    @Override
+    @KafkaListener(topics = "order-stream", groupId = "orders")
+    public void orderMessageListener(OrderDTO order) {
+        log.info(order.toString());
+    }
+}
