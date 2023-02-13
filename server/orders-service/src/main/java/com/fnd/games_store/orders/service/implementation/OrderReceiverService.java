794e02bc5293fc4976f476196646bae5edaeb0dc
SergeyPodgorny <serkonpod@gmail.com>
2023-02-13 18:22:37 +0300

    removed unused imports in OrderReceiverService

--- server/orders-service/src/main/java/com/fnd/games_store/orders/service/implementation/OrderReceiverService.java
+++ server/orders-service/src/main/java/com/fnd/games_store/orders/service/implementation/OrderReceiverService.java
@@ -3,15 +3,10 @@ package com.fnd.games_store.orders.service.implementation;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fnd.games_store.orders.dto.OrderDTO;
-import com.fnd.games_store.orders.service.OrderMessageListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @Slf4j
