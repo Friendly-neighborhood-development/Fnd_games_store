b5f9ba2205185970f5f19c0483cab84eaf55755a
SergeyPodgorny <serkonpod@gmail.com>
2023-02-16 19:28:06 +0300

    database schema migration script updated

--- server/orders-service/src/main/resources/db/migration/V1__create_scheme.sql
+++ server/orders-service/src/main/resources/db/migration/V1__create_scheme.sql
@@ -6,12 +6,21 @@ is_order_processed boolean NOT NULL);
 
 CREATE TABLE IF NOT EXISTS games
 (game_id varchar(255) PRIMARY KEY UNIQUE NOT NULL,
-order_id varchar(255),
 name varchar(255) UNIQUE NOT NULL,
 release_date varchar(50) NOT NULL,
 price numeric(10,2) NOT NULL,
 discount numeric(10,2) NOT NULL,
 description varchar NOT NULL,
-base64Image varchar NOT NULL,
+base64Image varchar NOT NULL);
 
-FOREIGN KEY(order_id) REFERENCES orders(order_id));
+CREATE TABLE IF NOT EXISTS order_game
+(order_id varchar(255),
+game_id varchar(255),
+
+
+
+FOREIGN KEY order_id REFERENCES order_id(orders),
+FOREIGN KEY game_id, REFERENCES game_id(games)
+
+);
+
