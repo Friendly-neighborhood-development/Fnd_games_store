2743046e0aa55c33b956cd119118e573f28315bc
SergeyPodgorny <serkonpod@gmail.com>
2023-02-16 19:23:16 +0300

    OrderSaver test case added

---
+++ server/orders-service/src/test/java/com/fnd/games_store/orders/service/SaveOrderTest.java
@@ -0,0 +1,18 @@
+package com.fnd.games_store.orders.service;
+
+
+import com.fnd.games_store.orders.OrdersApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = OrdersApplication.class)
+public class SaveOrderTest {
+
+
+
+
+
+
+
+
+
+}
