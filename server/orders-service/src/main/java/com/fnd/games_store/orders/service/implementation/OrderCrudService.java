fdefea227c605043d5f40db09010b0c413b7447e
SergeyPodgorny <serkonpod@gmail.com>
2023-02-16 19:21:28 +0300

    OrderSaver implementation deployed

---
+++ server/orders-service/src/main/java/com/fnd/games_store/orders/service/implementation/OrderCrudService.java
@@ -0,0 +1,17 @@
+package com.fnd.games_store.orders.service.implementation;
+
+import com.fnd.games_store.orders.dto.OrderRequestDTO;
+import com.fnd.games_store.orders.service.OrderSaver;
+
+public class OrderCrudService implements OrderSaver {
+
+
+
+    @Override
+    public void saveOrder(OrderRequestDTO order) {
+
+    }
+
+
+
+}
