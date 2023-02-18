fd5a1623b0ca6f192a6177190f5e01404ba246aa
SergeyPodgorny <serkonpod@gmail.com>
2023-02-18 06:02:17 +0300

    OrderCrudService renamed to more conventional name

--- server/orders-service/src/main/java/com/fnd/games_store/orders/service/implementation/OrderSaverService.java
+++ server/orders-service/src/main/java/com/fnd/games_store/orders/service/implementation/OrderSaverService.java
@@ -3,7 +3,7 @@ package com.fnd.games_store.orders.service.implementation;
 import com.fnd.games_store.orders.dto.OrderRequestDTO;
 import com.fnd.games_store.orders.service.OrderSaver;
 
-public class OrderCrudService implements OrderSaver {
+public class OrderSaverService implements OrderSaver {
 
 
 
