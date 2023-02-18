0ffe720d77b44fc5b3245bc46f6ab4003ae2421c
SergeyPodgorny <serkonpod@gmail.com>
2023-02-18 05:35:21 +0300

    injected entityManager and OrderRepository to SaveOrder test case

--- server/orders-service/src/test/java/com/fnd/games_store/orders/service/SaveOrderTest.java
+++ server/orders-service/src/test/java/com/fnd/games_store/orders/service/SaveOrderTest.java
@@ -2,7 +2,12 @@ package com.fnd.games_store.orders.service;
 
 
 import com.fnd.games_store.orders.OrdersApplication;
+import com.fnd.games_store.orders.repository.OrderRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
 
 @SpringBootTest(classes = OrdersApplication.class)
 public class SaveOrderTest {
@@ -12,6 +17,32 @@ public class SaveOrderTest {
 
 
 
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+
+
+
+
+
+
+
+
+
+
+    @BeforeEach
+    void testSetup(){
+
+
+
+
+
+
+
+    }
 
 
 
