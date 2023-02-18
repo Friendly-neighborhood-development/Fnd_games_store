fac7237e152d75ada9d0b0ff12b9d95364e3ccd7
SergeyPodgorny <serkonpod@gmail.com>
2023-02-18 08:00:28 +0300

    saveOrder() implementation provided

--- server/orders-service/src/main/java/com/fnd/games_store/orders/service/OrderSaver.java
+++ server/orders-service/src/main/java/com/fnd/games_store/orders/service/OrderSaver.java
@@ -5,6 +5,6 @@ import com.fnd.games_store.orders.dto.OrderRequestDTO;
 public interface OrderSaver {
 
 
-    void saveOrder(OrderRequestDTO order);
+    void saveOrder(OrderRequestDTO incomingOrder);
 
 }
--- server/orders-service/src/main/java/com/fnd/games_store/orders/service/implementation/OrderSaverService.java
+++ server/orders-service/src/main/java/com/fnd/games_store/orders/service/implementation/OrderSaverService.java
@@ -1,14 +1,35 @@
 package com.fnd.games_store.orders.service.implementation;
 
 import com.fnd.games_store.orders.dto.OrderRequestDTO;
+import com.fnd.games_store.orders.entity.Game;
+import com.fnd.games_store.orders.entity.Order;
+import com.fnd.games_store.orders.repository.OrderRepository;
 import com.fnd.games_store.orders.service.OrderSaver;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.stream.Collectors;
 
 public class OrderSaverService implements OrderSaver {
 
 
 
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderSaverService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
     @Override
-    public void saveOrder(OrderRequestDTO order) {
+    public void saveOrder(OrderRequestDTO incomingOrderData) {
+
+        Order savableOrderData = new Order();
+
+        savableOrderData.setIsOrderProcessed(true);
+        savableOrderData.setUserId(incomingOrderData.getUserId());
+        savableOrderData.setGames(incomingOrderData.getGameData().stream().map((Game::new)).collect(Collectors.toList()));
+
+        orderRepository.save(savableOrderData);
 
     }
 
