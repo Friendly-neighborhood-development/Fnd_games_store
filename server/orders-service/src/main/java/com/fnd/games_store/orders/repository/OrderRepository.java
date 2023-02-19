5079d07678daf9b37c746c22ca64200773fd0b05
SergeyPodgorny <serkonpod@gmail.com>
2023-02-19 15:14:35 +0300

    added repository method to determine if the order data es abscent in database

--- server/orders-service/src/main/java/com/fnd/games_store/orders/repository/OrderRepository.java
+++ server/orders-service/src/main/java/com/fnd/games_store/orders/repository/OrderRepository.java
@@ -3,5 +3,11 @@ package com.fnd.games_store.orders.repository;
 import com.fnd.games_store.orders.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrderRepository extends JpaRepository<Order, String> {
+
+    Optional<Order> findOrderByUserId(String userId);
+
+
 }
