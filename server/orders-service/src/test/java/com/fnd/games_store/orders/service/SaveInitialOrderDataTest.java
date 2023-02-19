92a053e933f7c1f2e5c8b2abae66d45a4ce1bd90
SergeyPodgorny <serkonpod@gmail.com>
2023-02-19 17:06:45 +0300

    testcase refactored

--- server/orders-service/src/test/java/com/fnd/games_store/orders/service/SaveInitialOrderDataTest.java
+++ server/orders-service/src/test/java/com/fnd/games_store/orders/service/SaveInitialOrderDataTest.java
@@ -3,6 +3,7 @@ package com.fnd.games_store.orders.service;
 
 import com.fnd.games_store.orders.OrdersApplication;
 import com.fnd.games_store.orders.dto.OrderRequestDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,10 +14,13 @@ import java.util.ArrayList;
 import static org.assertj.core.api.Assertions.*;
 @Transactional
 @SpringBootTest(classes = OrdersApplication.class)
+@Slf4j
 public class SaveInitialOrderDataTest extends ServiceTestUtils{
 
     @Test
     void saveOrder_ShouldCreateInitialOrderEntry(){
+        log.info("saved order: "+savedOrder.toString());
+        log.info("expected order: "+expectedOrder.toString());
         assertThat(savedOrder).isEqualTo(expectedOrder);
     }
 
@@ -26,6 +30,8 @@ public class SaveInitialOrderDataTest extends ServiceTestUtils{
         savedOrder.setIsOrderProcessed(true);
         savedOrder.setUserId(userId);
         savedOrder.setOrderDate(testCaseInitTime);
+
+        testgameList.add(createTestGameEntity("1"));
 
         savedOrder.setGames(testgameList);
 
