bea50bdd339b500b019961e3d55236ba0bac8b9d
SergeyPodgorny <serkonpod@gmail.com>
2023-02-20 19:52:54 +0300

    test case for order controller is under development

--- server/orders-service/src/main/java/com/fnd/games_store/orders/dto/OrderResponseDTO.java
+++ server/orders-service/src/main/java/com/fnd/games_store/orders/dto/OrderResponseDTO.java
@@ -1,10 +1,17 @@
 package com.fnd.games_store.orders.dto;
 
 import com.fnd.games_store.orders.entity.Order;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class OrderResponseDTO {
 
 
---
+++ server/orders-service/src/test/java/com/fnd/games_store/orders/controller/GetOrderDataTest.java
@@ -0,0 +1,79 @@
+package com.fnd.games_store.orders.controller;
+
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fnd.games_store.orders.dto.GameResponseDTO;
+import com.fnd.games_store.orders.dto.OrderResponseDTO;
+import com.fnd.games_store.orders.entity.Game;
+import com.fnd.games_store.orders.service.OrderFetcher;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.when;
+@SpringBootTest(classes = OrderController.class)
+@AutoConfigureMockMvc
+public class GetOrderDataTest {
+
+
+
+    @MockBean
+    protected OrderFetcher service;
+
+    @Autowired
+    protected MockMvc mvc;
+
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    protected String userId = "user1";
+
+
+    protected OrderResponseDTO mockedOrderResponse = new OrderResponseDTO();;
+
+    protected List<GameResponseDTO> testGameList = new ArrayList<>();
+
+
+    private OffsetDateTime testCaseInitTime = OffsetDateTime.now();
+
+
+    @BeforeEach
+    void testSetup(){
+
+        testGameList.add(createTestGameEntity("1"));
+        mockedOrderResponse.setGameData(testGameList);
+
+        when(service.fetchOrderData(userId)).thenReturn(mockedOrderResponse);
+
+    }
+
+
+    protected GameResponseDTO createTestGameEntity(String differenceParameter){
+
+        GameResponseDTO testGameEntity = new GameResponseDTO();
+        testGameEntity.setId(differenceParameter);
+        testGameEntity.setName("Doom Eternal" + differenceParameter);
+        testGameEntity.setReleaseDate("2009"+differenceParameter);
+        testGameEntity.setPrice(BigDecimal.valueOf(1000));
+        testGameEntity.setDiscount(BigDecimal.valueOf(20));
+        testGameEntity.setDescription(""+differenceParameter);
+        testGameEntity.setBase64Image(""+differenceParameter);
+
+        return testGameEntity;
+    }
+
+
+}
