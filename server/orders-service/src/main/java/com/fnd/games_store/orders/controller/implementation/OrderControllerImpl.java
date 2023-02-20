331ac2234c21bcc944a75b512cd8bfa917edcdca
SergeyPodgorny <serkonpod@gmail.com>
2023-02-20 19:34:26 +0300

    remved unused import

--- server/orders-service/src/main/java/com/fnd/games_store/orders/controller/implementation/OrderControllerImpl.java
+++ server/orders-service/src/main/java/com/fnd/games_store/orders/controller/implementation/OrderControllerImpl.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.ws.Response;
 
 @RestController
 public class OrderControllerImpl implements OrderController {
