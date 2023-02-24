3d59004ebdbf5e96bd788db5c62d5481d6490af5
SergeyPodgorny <serkonpod@gmail.com>
2023-02-24 07:15:16 +0300

    orders service main class annotations updated

--- server/orders-service/src/main/java/com/fnd/games_store/orders/OrdersApplication.java
+++ server/orders-service/src/main/java/com/fnd/games_store/orders/OrdersApplication.java
@@ -2,10 +2,13 @@ package com.fnd.games_store.orders;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 @EnableDiscoveryClient
+@EnableFeignClients
 public class OrdersApplication {
 
     public static void main(String[] args) {
