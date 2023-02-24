223a247b85ab08defe1bd34486602816f217be8d
SergeyPodgorny <serkonpod@gmail.com>
2023-02-24 06:33:50 +0300

    test case for order controller is developed

--- server/orders-service/src/main/java/com/fnd/games_store/orders/controller/implementation/OrderControllerImpl.java
+++ server/orders-service/src/main/java/com/fnd/games_store/orders/controller/implementation/OrderControllerImpl.java
@@ -5,6 +5,8 @@ import com.fnd.games_store.orders.dto.OrderResponseDTO;
 import com.fnd.games_store.orders.service.OrderFetcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -18,8 +20,10 @@ public class OrderControllerImpl implements OrderController {
         this.service = service;
     }
 
+
     @Override
-    public ResponseEntity<OrderResponseDTO> getOrderData(String userId) {
+    @PostMapping("/v1/purchases/{userId}")
+    public ResponseEntity<OrderResponseDTO> getOrderData(@PathVariable String userId) {
         return ResponseEntity.ok(service.fetchOrderData(userId));
     }
 }
--- server/orders-service/src/test/java/com/fnd/games_store/orders/controller/GetOrderDataTest.java
+++ server/orders-service/src/test/java/com/fnd/games_store/orders/controller/GetOrderDataTest.java
@@ -5,24 +5,26 @@ package com.fnd.games_store.orders.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fnd.games_store.orders.dto.GameResponseDTO;
 import com.fnd.games_store.orders.dto.OrderResponseDTO;
-import com.fnd.games_store.orders.entity.Game;
 import com.fnd.games_store.orders.service.OrderFetcher;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(classes = OrderController.class)
 @AutoConfigureMockMvc
 public class GetOrderDataTest {
@@ -35,7 +37,6 @@ public class GetOrderDataTest {
     @Autowired
     protected MockMvc mvc;
 
-
     @Autowired
     protected ObjectMapper objectMapper;
 
@@ -46,8 +47,30 @@ public class GetOrderDataTest {
 
     protected List<GameResponseDTO> testGameList = new ArrayList<>();
 
-
     private OffsetDateTime testCaseInitTime = OffsetDateTime.now();
+
+    protected OrderResponseDTO expectedOrderData = new OrderResponseDTO();
+
+
+
+    @Test
+    void getDataEndpoint_ShouldReturnProperResponse() throws Exception {
+
+
+        MvcResult mvcResult = mvc.perform(post("/v1/purchases/"+userId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk()).andReturn();
+
+        String requestBody = mvcResult.getResponse().getContentAsString();
+
+        OrderResponseDTO actualOrderData = objectMapper.readValue(requestBody, OrderResponseDTO.class);
+
+        assertThat(actualOrderData).isEqualTo(expectedOrderData);
+
+    }
+
+
+
 
 
     @BeforeEach
@@ -55,6 +78,8 @@ public class GetOrderDataTest {
 
         testGameList.add(createTestGameEntity("1"));
         mockedOrderResponse.setGameData(testGameList);
+
+        expectedOrderData.setGameData(testGameList);
 
         when(service.fetchOrderData(userId)).thenReturn(mockedOrderResponse);
 
