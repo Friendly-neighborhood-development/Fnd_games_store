b7b2665f843e58b37c4415fbb55e725ea99a6e7c
SergeyPodgorny <serkonpod@gmail.com>
2023-02-24 07:22:59 +0300

    web security configuration deployed

---
+++ server/orders-service/src/main/java/com/fnd/games_store/orders/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.fnd.games_store.orders.configuration;
+
+
+
+import com.fnd.games_store.orders.filter.JwtFilter;
+import com.fnd.games_store.orders.rest.UserValidationClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filter(HttpSecurity http) throws Exception {
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+
+        http.formLogin().disable();
+
+        http.cors().disable();
+
+        http.csrf().disable();
+
+        return http.build();
+
+    }
+
+    @Bean
+    public JwtFilter globalFilter(UserValidationClient userValidationClient){
+        return new JwtFilter(userValidationClient);
+    }
+
+}
