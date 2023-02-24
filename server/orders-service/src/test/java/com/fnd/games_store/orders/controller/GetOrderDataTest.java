475e41e4f373eab4ebc5eeda75df991426f75540
SergeyPodgorny <serkonpod@gmail.com>
2023-02-24 07:42:20 +0300

    service now secured and fully tested

--- server/orders-service/src/test/java/com/fnd/games_store/orders/controller/GetOrderDataTest.java
+++ server/orders-service/src/test/java/com/fnd/games_store/orders/controller/GetOrderDataTest.java
@@ -3,12 +3,14 @@ package com.fnd.games_store.orders.controller;
 
 import com.fnd.games_store.orders.OrdersApplication;
 import com.fnd.games_store.orders.dto.OrderResponseDTO;
+import com.fnd.games_store.orders.dto.ValidationResponseDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,6 +28,7 @@ public class GetOrderDataTest extends ControllerTestUtils{
 
 
         MvcResult mvcResult = mvc.perform(get("/v1/purchases/"+userId)
+                        .header("authorization", "")
                         .contentType(MediaType.APPLICATION_JSON))
                         .andExpect(status().isOk()).andReturn();
 
@@ -50,6 +53,19 @@ public class GetOrderDataTest extends ControllerTestUtils{
 
         when(service.fetchOrderData(userId)).thenReturn(mockedOrderResponse);
 
+    }
+
+    @BeforeEach
+    void userValidationFeignClientSetup(){
+
+        Boolean tokenIsValid = true;
+
+        ValidationResponseDTO response = new ValidationResponseDTO();
+        response.setIsTokenValid(true);
+
+        ResponseEntity<ValidationResponseDTO> responseBody = ResponseEntity.ok(response);
+
+        when(userValidationClient.validateUser("")).thenReturn(responseBody);
     }
 
 }
