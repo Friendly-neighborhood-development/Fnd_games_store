fe3247b287fb28d2f4ab1f17f42240be2c60c664
SergeyPodgorny <serkonpod@gmail.com>
2023-03-12 07:43:44 +0300

    stable helthy status of the config-service obtained

--- server/config-service/src/main/java/com/fnd/games_store/config/ConfigApplication.java
+++ server/config-service/src/main/java/com/fnd/games_store/config/ConfigApplication.java
@@ -1,15 +1,22 @@
 package com.fnd.games_store.config;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import java.net.InetAddress;
+
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigApplication {
+public class ConfigApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("===================="+ InetAddress.getLocalHost().getHostAddress()+"=====================");
+    }
 }
