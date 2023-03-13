bdec7688a547199776f6ecd5a1dc33a16c9035e3
SergeyPodgorny <serkonpod@gmail.com>
2023-03-13 07:23:29 +0300

    config-service has been amde eureka client

--- server/config-service/src/main/java/com/fnd/games_store/config/ConfigApplication.java
+++ server/config-service/src/main/java/com/fnd/games_store/config/ConfigApplication.java
@@ -3,12 +3,14 @@ package com.fnd.games_store.config;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 import java.net.InetAddress;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
