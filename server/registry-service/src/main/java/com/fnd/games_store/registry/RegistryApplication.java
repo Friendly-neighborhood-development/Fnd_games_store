133f1256f9f8f4825b201e5aeb615cf45f18f6e7
SergeyPodgorny <serkonpod@gmail.com>
2023-03-13 07:14:35 +0300

    showing app ip address deployed to eureka

--- server/registry-service/src/main/java/com/fnd/games_store/registry/RegistryApplication.java
+++ server/registry-service/src/main/java/com/fnd/games_store/registry/RegistryApplication.java
@@ -1,13 +1,22 @@
 package com.fnd.games_store.registry;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.net.InetAddress;
+
 @SpringBootApplication
 @EnableEurekaServer
-public class RegistryApplication {
+public class RegistryApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(RegistryApplication.class, args);
     }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("===================="+ InetAddress.getLocalHost().getHostAddress()+"=====================");
+    }
+
 }
