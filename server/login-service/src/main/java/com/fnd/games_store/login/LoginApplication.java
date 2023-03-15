b33a00436c570f19e1063861e86876aec8f16213
SergeyPodgorny <serkonpod@gmail.com>
2023-03-15 16:32:35 +0300

    removed unused imports

--- server/login-service/src/main/java/com/fnd/games_store/login/LoginApplication.java
+++ server/login-service/src/main/java/com/fnd/games_store/login/LoginApplication.java
@@ -1,13 +1,11 @@
 package com.fnd.games_store.login;
 
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.security.core.context.SecurityContextHolder;
-import sun.tools.jar.CommandLine;
 
 import java.net.InetAddress;
 
