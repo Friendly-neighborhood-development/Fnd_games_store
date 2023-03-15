058885fda94ac7a7884563df18a13c6bf15024d7
SergeyPodgorny <serkonpod@gmail.com>
2023-03-15 15:51:00 +0300

    added ip adress startup detector to games-service

--- server/games-service/src/main/java/com/fnd/games_store/games/GamesApplication.java
+++ server/games-service/src/main/java/com/fnd/games_store/games/GamesApplication.java
@@ -1,5 +1,6 @@
 package com.fnd.games_store.games;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
@@ -7,15 +8,22 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.net.InetAddress;
+
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableCaching
 @EnableFeignClients
-public class GamesApplication {
+public class GamesApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(GamesApplication.class, args);
     }
 
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("===================="+ InetAddress.getLocalHost().getHostAddress()+"=====================");
+    }
 }
