13c07b3b4d3a5a566c8098dac0391b19d9ac954b
SergeyPodgorny <serkonpod@gmail.com>
2023-03-15 16:23:39 +0300

    Current ip address printer deployed

--- server/login-service/src/main/java/com/fnd/games_store/login/LoginApplication.java
+++ server/login-service/src/main/java/com/fnd/games_store/login/LoginApplication.java
@@ -7,15 +7,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.core.context.SecurityContextHolder;
+import sun.tools.jar.CommandLine;
+
+import java.net.InetAddress;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class LoginApplication {
+public class LoginApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(LoginApplication.class, args);
 
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("===================="+ InetAddress.getLocalHost().getHostAddress()+"=====================");
     }
 
 }
