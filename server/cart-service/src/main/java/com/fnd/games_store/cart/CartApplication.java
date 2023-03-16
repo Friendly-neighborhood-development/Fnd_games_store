55435340b1a63e0b3036418864c5e12bdc5bf6b8
SergeyPodgorny <serkonpod@gmail.com>
2023-03-16 19:02:28 +0300

    cart-service adjusted to work with redis in compose environment

--- server/cart-service/src/main/java/com/fnd/games_store/cart/CartApplication.java
+++ server/cart-service/src/main/java/com/fnd/games_store/cart/CartApplication.java
@@ -1,6 +1,11 @@
 package com.fnd.games_store.cart;
 
 
+import com.fnd.games_store.cart.entity.Cart;
+import com.fnd.games_store.cart.entity.Game;
+import com.fnd.games_store.cart.repository.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -8,14 +13,21 @@ import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServic
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableFeignClients
-public class CartApplication {
+public class CartApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(CartApplication.class,args);
     }
 
-
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("===================="+ InetAddress.getLocalHost().getHostAddress()+"=====================");
+    }
 }
