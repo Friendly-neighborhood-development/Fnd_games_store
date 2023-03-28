b51688031dcf633b79c9d59151c754d1af2cc862
SergeyPodgorny <serkonpod@gmail.com>
2023-03-28 19:50:50 +0300

    controlle test refactored

--- server/orders-service/src/test/java/com/fnd/games_store/orders/controller/GetOrderDataTest.java
+++ server/orders-service/src/test/java/com/fnd/games_store/orders/controller/GetOrderDataTest.java
@@ -1,6 +1,7 @@
 package com.fnd.games_store.orders.controller;
 
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fnd.games_store.orders.OrdersApplication;
 import com.fnd.games_store.orders.dto.OrderResponseDTO;
 import com.fnd.games_store.orders.dto.ValidationResponseDTO;
@@ -13,6 +14,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -23,49 +27,52 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class GetOrderDataTest extends ControllerTestUtils{
 
 
-//    @Test
-//    void getDataEndpoint_ShouldReturnProperResponse() throws Exception {
-//
-//
-//        MvcResult mvcResult = mvc.perform(get("/v1/purchases/"+userId)
-//                        .header("authorization", "")
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                        .andExpect(status().isOk()).andReturn();
-//
-//        String requestBody = mvcResult.getResponse().getContentAsString();
-//
-//        OrderResponseDTO actualOrderData = objectMapper.readValue(requestBody, OrderResponseDTO.class);
-//
-//        log.info("actual order data: "+actualOrderData.toString());
-//        log.info("expected order data: "+expectedOrderData.toString());
-//
-//        assertThat(actualOrderData).isEqualTo(expectedOrderData);
-//
-//    }
-//
-//    @BeforeEach
-//    void testSetup(){
-//
-//        testGameList.add(createTestGameEntity("1"));
-//        mockedOrderResponse.setGameData(testGameList);
-//
-//        expectedOrderData.setGameData(testGameList);
-//
-//        when(service.fetchOrderData(userId)).thenReturn(mockedOrderResponse);
-//
-//    }
-//
-//    @BeforeEach
-//    void userValidationFeignClientSetup(){
-//
-//        Boolean tokenIsValid = true;
-//
-//        ValidationResponseDTO response = new ValidationResponseDTO();
-//        response.setIsTokenValid(true);
-//
-//        ResponseEntity<ValidationResponseDTO> responseBody = ResponseEntity.ok(response);
-//
-//        when(userValidationClient.validateUser("")).thenReturn(responseBody);
-//    }
+    @Test
+    void getDataEndpoint_ShouldReturnProperResponse() throws Exception {
+
+
+        MvcResult mvcResult = mvc.perform(get("/v1/purchases/"+userId)
+                        .header("authorization", "")
+                        .contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk()).andReturn();
+
+        String requestBody = mvcResult.getResponse().getContentAsString();
+
+        List<OrderResponseDTO> actualOrderData = objectMapper.readValue(requestBody, new TypeReference<List<OrderResponseDTO>>() {});
+
+        log.info("actual order data: "+actualOrderData.toString());
+        log.info("expected order data: "+expectedOrderData.getGameData().toString());
+
+        assertThat(actualOrderData.get(0)).isEqualTo(expectedOrderData);
+
+    }
+
+    @BeforeEach
+    void testSetup(){
+
+        testGameList.add(createTestGameEntity("1"));
+
+
+
+        mockedOrderResponse.add(expectedOrderData);
+
+        expectedOrderData.setGameData(testGameList);
+
+        when(service.fetchOrderData(userId)).thenReturn(mockedOrderResponse);
+
+    }
+
+    @BeforeEach
+    void userValidationFeignClientSetup(){
+
+        Boolean tokenIsValid = true;
+
+        ValidationResponseDTO response = new ValidationResponseDTO();
+        response.setIsTokenValid(true);
+
+        ResponseEntity<ValidationResponseDTO> responseBody = ResponseEntity.ok(response);
+
+        when(userValidationClient.validateUser("")).thenReturn(responseBody);
+    }
 
 }
