8b792584e8be25aad63936ad546ce4b9081c1461
SergeyPodgorny <serkonpod@gmail.com>
2023-04-09 11:14:12 +0300

    shell script for config-service build deployed

--- low_ram_build.sh
+++ low_ram_build.sh
@@ -8,5 +8,10 @@ GREEN='\033[0;32m'
 printf ${YELLOW}"===Building registry-service===/n"${NC}
 cd server/registry-service;
 docker build -t registry-service .;
-printf ${GREEN}"===Registry-service build completed==="${NC}
+printf ${GREEN}"===Registry-service build completed===/n"${NC}
 
+printf ${YELLOW}"===Building config-service===/n"${NC}
+cd ..
+cd config-service;
+docker build -t config-service .;
+printf ${GREEN}"===Config-service build completed===/n"${NC}
