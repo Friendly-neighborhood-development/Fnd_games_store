bbecbd5be770d000df10628855e2bd6ef508e89f
SergeyPodgorny <serkonpod@gmail.com>
2023-04-09 11:41:29 +0300

    scripts for build whole app deployed

--- low_ram_build.sh
+++ low_ram_build.sh
@@ -11,5 +11,15 @@ docker build -t config-service ./server/config-service;
 
 docker build -t gateway-service ./server/gateway-service;
 
+docker build -t games-service ./server/games-service;
+
+docker build -t login-service ./server/login-service;
+
+docker build -t orders-service ./server/orders-service;
+
+docker build -t cart-service ./server/cart-service;
+
+docker build -t client-application ./client
+
 
 printf ${GREEN}"===Fnd_games_store build completed===/n"${NC}
