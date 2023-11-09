044b4fca80b5036ebe4038c2d46aa298e9c851aa
SergeyPodgorny <serkonpod@gmail.com>
2023-11-09 10:15:29 +0300

    transaction annotation parameters for userLogin updated and refactored

--- server/login-service/src/main/java/com/fnd/games_store/login/service/implementation/LoginServiceImpl.java
+++ server/login-service/src/main/java/com/fnd/games_store/login/service/implementation/LoginServiceImpl.java
@@ -2,6 +2,7 @@ package com.fnd.games_store.login.service.implementation;
 
 
 import com.fnd.games_store.login.dto.LoginResponseDTO;
+import com.fnd.games_store.login.exception.AccountNotFoundException;
 import com.fnd.games_store.login.exception.InvalidPasswordException;
 import com.fnd.games_store.login.jwt_utils.JwtGenerator;
 import com.fnd.games_store.login.repository.AccountRepository;
@@ -12,11 +13,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
-@Transactional(readOnly = true)
 public class LoginServiceImpl implements LoginService {
 
     private final PasswordEncoder passwordEncoder;
@@ -32,7 +34,12 @@ public class LoginServiceImpl implements LoginService {
         this.jwtGenerator = jwtGenerator;
         this.accountRepository = accountRepository;
     }
-
+    @Transactional(label = "user_login",
+            propagation = Propagation.REQUIRED,
+            isolation = Isolation.REPEATABLE_READ,
+            timeout = 5,
+            readOnly = true,
+            rollbackFor = {AccountNotFoundException.class, InvalidPasswordException.class})
     @Override
     public LoginResponseDTO login(String username, String password) {
 
