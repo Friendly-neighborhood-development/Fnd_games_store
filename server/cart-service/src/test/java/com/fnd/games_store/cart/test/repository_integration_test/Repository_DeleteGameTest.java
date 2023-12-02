9f45f66dd18c2f89315db41b0d324b6ea8bb055e
SergeyPodgorny <serkonpod@gmail.com>
2023-12-02 06:53:21 +0300

    testcontainer for redis deployed

--- server/cart-service/src/test/java/com/fnd/games_store/cart/test/repository_integration_test/Repository_DeleteGameTest.java
+++ server/cart-service/src/test/java/com/fnd/games_store/cart/test/repository_integration_test/Repository_DeleteGameTest.java
@@ -4,21 +4,47 @@ package com.fnd.games_store.cart.test.repository_integration_test;
 import com.fnd.games_store.cart.test.utilities.RepositoryTestUtilities;
 
 import com.fnd.games_store.cart.CartApplication;
+import com.redis.testcontainers.RedisContainer;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 
 @SpringBootTest(classes = CartApplication.class)
 @Slf4j
+@Testcontainers(disabledWithoutDocker = true)
 public class Repository_DeleteGameTest extends RepositoryTestUtilities {
 
+
+    @Container
+    private static final RedisContainer REDIS_CONTAINER = new RedisContainer(DockerImageName.parse("redis:5.0.3-alpine")).withExposedPorts(6379);
+
+
+    @DynamicPropertySource
+    private static void registerRedisProperties(DynamicPropertyRegistry registry) {
+        registry.add("variables.redis.host", REDIS_CONTAINER::getHost);
+        registry.add("variables.redis.port", () -> REDIS_CONTAINER.getMappedPort(6379)
+                .toString());
+    }
+
+
     @Test
-    void deleteGameInCart_ShouldDeleteGameEntry(){
+    void givenRedisContainerConfiguredWithDynamicProperties_whenCheckingRunningStatus_thenStatusIsRunning() {
+        assertTrue(REDIS_CONTAINER.isRunning());
+    }
+
+
+//    @Test
+//    void deleteGameInCart_ShouldDeleteGameEntry(){
 
 //        testCartGameData.remove((createTestGameEntity("1")));
 //        repository.save(testCart);
@@ -28,7 +54,7 @@ public class Repository_DeleteGameTest extends RepositoryTestUtilities {
 //        log.info("cart persisted in db: "+repository.findById(userId).get().toString());
 //        log.info("updated cart: "+testCart.toString());
 
-    }
+//    }
 
 
 //    @BeforeEach
